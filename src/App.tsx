dd40d587e13df82a73c5544ad6572fd5ed321d53
robertschaap <robert.schaap@mac.com>
2019-10-20 09:15:29 +0200

    App: add global font import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
 import Root from './containers/Root';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <Root />
+    <>
+      <Root />
+      <GlobalStyle />
+    </>
   );
 }
 
