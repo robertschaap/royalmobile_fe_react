a942af6049dcc24798214d73b23787197edfcb1a
robertschaap <robert.schaap@mac.com>
2019-10-22 19:04:01 +0200

    Banner: setup base component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner: React.FC = () => {
+  return (
+    <section>
+      <span>Fat Freaking Discounts</span>
+      <span>SALE</span>
+    </section>
+  );
+};
+
+export default Banner;
