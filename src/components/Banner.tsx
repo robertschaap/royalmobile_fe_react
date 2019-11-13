ce5d1eefb28aed8e24c2499d39cac119dfc48a3e
robertschaap <robert.schaap@mac.com>
2019-11-13 20:22:35 +0100

    Banner: setup base styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const BannerBase = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Banner: React.FC = () => {
   return (
-    <section>
+    <BannerBase>
       <span>Fat Freaking Discounts</span>
       <span>SALE</span>
-    </section>
+    </BannerBase>
   );
 };
 
