93386c4be48594967281774ca21f1ad46ea3b3c0
robertschaap <robert.schaap@mac.com>
2019-11-14 22:11:47 +0100

    App: add box-sizing to global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ import { createGlobalStyle } from 'styled-components';
 import Root from './containers/Root';
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
   body {
     font-family: 'Raleway', sans-serif;
   }
