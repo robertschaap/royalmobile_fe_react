6465dbf9865f1052ecf3e82cfc45c03a198e1643
robertschaap <robert.schaap@mac.com>
2019-11-14 21:10:17 +0100

    theme: setup breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,17 @@
 const spacing = (n: number) => (n * 4) + "px";
 
+const breakpoints = {
+  sm: 425,
+  md: 768,
+  lg: 1024,
+};
+
+export const breakpoint_up = {
+  sm: `@media (min-width: ${breakpoints.sm}x)`,
+  md: `@media (min-width: ${breakpoints.md}px)`,
+  lg: `@media (min-width: ${breakpoints.lg}px)`,
+}
+
 const color = {
   primary: '#ff9e00',
   secondary: '#c3ebeb',
@@ -12,6 +24,7 @@ const color = {
 const theme = {
   color,
   spacing,
+  breakpoints,
 };
 
 
