b428983d8443c1d01eb9ed0816a079f700a5a4c7
robertschaap <robert.schaap@mac.com>
2019-11-14 20:30:55 +0100

    theme: setup basic theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const color = {
+  primary: '#ff9e00',
+  secondary: '#c3ebeb',
+  grey: '#ddd',
+  lightGrey: '#eee',
+  black: 'black',
+  white: 'white',
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
