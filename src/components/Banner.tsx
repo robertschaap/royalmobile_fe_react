bab12699b5287b714c6334849f018840e857f1dd
robertschaap <robert.schaap@mac.com>
2019-11-14 22:13:35 +0100

    Banner: setup styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
+import { breakpoint_up } from '../styles/theme';
 
 const BannerBase = styled.section`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
+  align-items: center;
+  color: ${({ theme }) => theme.color.white};
+
+  padding: ${({ theme }) => theme.spacing(4)};
+  border-radius: 8px;
+  height: 96px;
+
+  background: rgb(255,142,0);
+  background: linear-gradient(
+    163deg,
+    ${({ theme }) => theme.color.primary} 0%,
+    ${({ theme }) => theme.color.secondary} 120%
+  );
+
+  @media ${breakpoint_up.md} {
+    justify-content: space-between;
+    padding: ${({ theme }) => theme.spacing(8)};
+    height: 200px;
+  }
+`;
+
+const BannerLeft = styled.span`
+    font-size: 32px;
+    font-weight: 700;
+    text-align: right;
+
+    @media ${breakpoint_up.md} {
+      text-align: unset;
+      font-size: 40px;
+    }
+`;
+
+const BannerRight = styled.span`
+    font-size: 111px;
+    font-weight: 700;
+    text-shadow: 0px 2px 2px rgba(1,1,1,0.16);
+    display: none;
+
+    @media ${breakpoint_up.md} {
+      display: unset;
+    }
 `;
 
 const Banner: React.FC = () => {
   return (
     <BannerBase>
-      <span>Fat Freaking Discounts</span>
-      <span>SALE</span>
+      <BannerLeft>Fat Freaking<br />Discounts</BannerLeft>
+      <BannerRight>SALE</BannerRight>
     </BannerBase>
   );
 };
