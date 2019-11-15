5d9c4fd26fd86f2026ac1886fe244c0eab8d1561
robertschaap <robert.schaap@mac.com>
2019-11-15 22:49:17 +0100

    Button: add variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ButtonBase = styled.button`
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 8px;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
+`;
+
+const ButtonPrimary = styled(ButtonBase)`
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
+const ButtonSecondary = styled(ButtonBase)`
+  background-color: ${({ theme }) => theme.color.lightGrey};
 `;
 
 interface ButtonProps {
   children: React.ReactNode;
+  variant: "primary" | "secondary";
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const { children } = props;
+  const { variant, ...rest } = props;
+
+  if (variant === "primary") {
+    return (
+      <ButtonPrimary {...rest} />
+    );
+  }
 
   return (
-    <ButtonBase>
-      {children}
-    </ButtonBase>
-  );
+    <ButtonSecondary {...rest} />
+  )
 };
 
 export default Button;
