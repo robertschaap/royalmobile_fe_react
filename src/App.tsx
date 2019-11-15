3b15db011d228ff3db627fc38e0617ca1115e405
robertschaap <robert.schaap@mac.com>
2019-11-15 21:48:05 +0100

    App: move styled components to Root

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 
 import Root from './containers/Root';
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    font-family: 'Raleway', sans-serif;
-  }
-`;
-
 const App: React.FC = () => {
   return (
-    <>
       <Root />
-      <GlobalStyle />
-    </>
   );
 }
 
--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
 import theme from '../styles/theme';
 
 import Banner from '../components/Banner';
 import RootLayout from '../components/RootLayout';
 
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+`;
+
 const Root: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <RootLayout>
-        <Banner />
-      </RootLayout>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <RootLayout>
+          <Banner />
+        </RootLayout>
+      </ThemeProvider>
+      <GlobalStyle />
+    </>
   );
 };
 
