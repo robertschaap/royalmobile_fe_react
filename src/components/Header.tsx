5c67c4ac9da659ed0822dd69318fb9bd82648cbe
robertschaap <robert.schaap@mac.com>
2019-11-15 19:37:42 +0100

    Header: add bottom-margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { breakpoint_up } from '../styles/theme';
 
 import Breadcrumbs from './Breadcrumbs';
 import HeaderLogo from './HeaderLogo';
 import Navigation from './Navigation';
 
+const HeaderBase = styled.header`
+  margin-bottom: ${({ theme }) => theme.spacing(4)}
+
+  @media ${breakpoint_up.md} {
+    margin-bottom: ${({ theme }) => theme.spacing(6)};
+  }
+`;
+
 const Header: React.FC = () => {
   return (
-    <header>
+    <HeaderBase>
       <HeaderLogo />
       <Navigation />
       <Breadcrumbs />
-    </header>
+    </HeaderBase>
   );
 };
 
