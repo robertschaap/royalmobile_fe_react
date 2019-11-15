d3ce1c4fcc74e67c430c47629829e0988379fb11
robertschaap <robert.schaap@mac.com>
2019-11-15 19:28:39 +0100

    Footer: add styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterBase = styled.footer`
-  background-color: ${({ theme }) => theme.color.blaat};
+  background-color: ${({ theme }) => theme.color.lightGrey};
+  border-radius: 8px 8px 0 0;
+  padding: ${({ theme }) => theme.spacing(4)};
 `;
 
 const Footer: React.FC = () => {
