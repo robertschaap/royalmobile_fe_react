4f1e57a9c080b30656678703fae96fb12dc88d0c
robertschaap <robert.schaap@mac.com>
2019-11-15 19:31:04 +0100

    RootLayout: add styling

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { breakpoint_up } from '../styles/theme';
 
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 import Banner from './Banner';
+
+const RootLayoutBase = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 ${({ theme }) => theme.spacing(4)};
+
+  @media ${breakpoint_up.md} {
+    padding: 0 ${({ theme }) => theme.spacing(8)};
+  }
+`;
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -13,14 +25,14 @@ const RootLayout: React.FC<RootLayoutProps> = (props) => {
   const { children } = props;
 
   return (
-    <>
+    <RootLayoutBase>
       <Header />
       <Main>
         {children}
         <Banner />
       </Main>
       <Footer />
-    </>
+    </RootLayoutBase>
   );
 };
 
