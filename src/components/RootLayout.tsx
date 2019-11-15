4e5d54270ec162b80fb7b5302be85c42b2eaada7
robertschaap <robert.schaap@mac.com>
2019-11-15 19:30:38 +0100

    RootLayout: add Banner component

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import Banner from './Banner';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -16,6 +17,7 @@ const RootLayout: React.FC<RootLayoutProps> = (props) => {
       <Header />
       <Main>
         {children}
+        <Banner />
       </Main>
       <Footer />
     </>
