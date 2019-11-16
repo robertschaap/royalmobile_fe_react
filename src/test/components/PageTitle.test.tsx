e8c10d5b87e83f848a19e2081544232ee99fee22
robertschaap <robert.schaap@mac.com>
2019-11-16 09:33:02 +0100

    test(PageTitle): remove reactDOM render

--- src/test/components/PageTitle.test.tsx
+++ src/test/components/PageTitle.test.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+
 import PageTitle from '../../components/PageTitle';
 
 describe('<PageTitle />', () => {
   it('should set the default title if no props are passed', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageTitle />, div);
+    render(<PageTitle />);
 
     expect(document.title).toBe("RoyalMobile");
   });
 
   it('should change the site title if passed', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageTitle title="Test" />, div);
+    render(<PageTitle title="Test"/>);
 
     expect(document.title).toBe("Test");
   });
 
   it('it should add the page title if passed', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageTitle page="Test" />, div);
+    render(<PageTitle page="Test" />);
 
     expect(document.title).toBe("RoyalMobile - Test");
   });
 
   it('it should change the site and page title if passed', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageTitle title="Test" page="Test" />, div);
+    render(<PageTitle title="Test" page="Test" />);
 
     expect(document.title).toBe("Test - Test");
   });
 });
-
-
-// pass title
-// pass page
-// pass nothing
