541a61decd2108cf93c53795c668fad06ea435aa
robertschaap <robert.schaap@mac.com>
2019-11-16 14:19:07 +0100

    feat(HomePageContainer): replace components with HomePage

--- src/containers/HomePageContainer.tsx
+++ src/containers/HomePageContainer.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import Banner from '../components/Banner';
-import Button from '../components/Button';
 import PageTitle from '../components/PageTitle';
+import HomePage from '../components/HomePage';
 
 const Main: React.FC = () => {
-
   return (
     <>
       <PageTitle page="Home" />
-      <Banner />
-      <Button variant="secondary">Load More</Button>
+      <HomePage />
     </>
   );
 };
