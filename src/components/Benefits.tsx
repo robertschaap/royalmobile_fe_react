5c6bfc2aa6b2ca1524d7bd9147b7ca218a4f1539
robertschaap <robert.schaap@mac.com>
2019-11-17 09:24:52 +0100

    feat(Benefits): setup component

---
+++ src/components/Benefits.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { breakpoint_up } from '../styles/theme';
+
+import { GridBase, GridItem } from '../components/Grid';
+
+const BenefitsItem = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: ${({ theme })=> theme.spacing(3)} ${({ theme })=> theme.spacing(4)};
+  border: 1px solid ${({ theme })=> theme.color.lightGrey};
+  border-radius: 8px;
+
+  @media ${breakpoint_up.md} {
+    padding: ${({ theme })=> theme.spacing(6)} ${({ theme })=> theme.spacing(8)};
+  }
+`;
+
+const BenefitsItemCallout = styled.span`
+  flex: none;
+  margin-right: ${({ theme })=> theme.spacing(4)};
+  color: ${({ theme }) => theme.color.primary};
+  font-size: 50px;
+  font-weight: 700;
+
+  @media ${breakpoint_up.md} {
+    margin-right: ${({ theme })=> theme.spacing(6)};
+  }
+`;
+
+const Benefits: React.FC = () => {
+  return (
+    <GridBase as="section">
+      <GridItem
+        md={2}
+        lg={2}>
+        <BenefitsItem>
+          <BenefitsItemCallout>GET</BenefitsItemCallout>
+          <span>Three months free inside of your regular bundle when you refer a friend</span>
+        </BenefitsItem>
+      </GridItem>
+      <GridItem
+        md={2}
+        lg={2}>
+        <BenefitsItem>
+          <BenefitsItemCallout>GET</BenefitsItemCallout>
+          <span>Double data in your bundle plus a 5 euro discount when you combine with <b>Royal</b>Internet</span>
+        </BenefitsItem>
+      </GridItem>
+  </GridBase>
+  );
+};
+
+export default Benefits;
