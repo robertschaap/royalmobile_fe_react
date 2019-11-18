7fa127d5c758911d638d7e073356bf537befaa87
robertschaap <robert.schaap@mac.com>
2019-11-18 17:09:58 +0100

    feat(HomePage): add ProductListing component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,37 +1,25 @@
 import React from 'react';
 
-import { GridBase, GridItem } from '../components/Grid';
 import Banner from '../components/Banner';
 import Benefits from '../components/Benefits';
 import Button from '../components/Button';
+import ProductListing from '../components/ProductListing';
 
-const devices = [
+const products = [
   { id: 1, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
-  { id: 2, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
-  { id: 3, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
-  { id: 4, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
-  { id: 5, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
-  { id: 6, name: "iPhoneX 16gb", manufacturer: "Apple", price: "265" },
+  { id: 2, name: "iPhone 11 Pro 128gb", manufacturer: "Apple", price: "265" },
+  { id: 3, name: "Galaxy Note 10 128gb", manufacturer: "Samsung", price: "265" },
+  { id: 4, name: "Galaxy s10 Plus 128gb", manufacturer: "Samsung", price: "265" },
+  { id: 5, name: "Lumia 8", manufacturer: "Nokia", price: "265" },
+  { id: 6, name: "P30", manufacturer: "Huawei", price: "265" },
 ];
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Banner />
-      <GridBase>
-        {devices.map(d => (
-          <GridItem
-            key={d.id}
-            xs={1}
-            sm={2}
-            md={3}
-            lg={4}>
-            <div>{d.name}</div>
-            <div>{d.manufacturer}</div>
-            <div>{d.price}</div>
-          </GridItem>
-        ))}
-      </GridBase>
+      <ProductListing
+        products={products} />
       <Button
         variant="secondary">
         Load More
