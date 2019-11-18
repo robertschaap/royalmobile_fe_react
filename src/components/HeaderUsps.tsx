4d69836b09af262da8971e6df1abf5c27b931a30
robertschaap <robert.schaap@mac.com>
2019-11-18 17:44:17 +0100

    feat(HeaderUsps): setup component

---
+++ src/components/HeaderUsps.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { breakpoint_up } from '../styles/theme';
+
+const HeaderUspsBase = styled.ul`
+  display: none;
+  text-align: right;
+  margin: ${({ theme }) => theme.spacing(2)} 0;
+  padding-left: 0;
+  list-style-type: none;
+
+  @media ${breakpoint_up.md} {
+    display: block;
+  }
+`;
+
+const Check = styled.span`
+  font-family: 'Lucida Grande';
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.primary};
+`;
+
+const HeaderUsps: React.FC = () => {
+  return (
+    <HeaderUspsBase>
+      <li><Check>✓</Check> Guaranteed no False Promises</li>
+      <li><Check>✓</Check> No Connection Fee For New Customers</li>
+      <li><Check>✓</Check> Free Shipping on All Orders</li>
+    </HeaderUspsBase>
+  );
+};
+
+export default HeaderUsps;
