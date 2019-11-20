251ba738fe2375c3554677dd231a8ed6ebe5e02b
robertschaap <robert.schaap@mac.com>
2019-11-20 23:13:59 +0100

    feat(OrderPageContainer): setup base component

---
+++ src/containers/OrderPageContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+
+const OrderPageContainer: React.FC = () => {
+  return (
+    <>
+      <PageTitle page="Order" />
+      <div>OrderPageContainer</div>
+    </>
+  );
+};
+
+export default OrderPageContainer;
