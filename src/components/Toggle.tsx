c0ab46f49fdfac3c74ebb189d821ac7660723ca1
robertschaap <robert.schaap@mac.com>
2019-11-20 18:37:06 +0100

    feat(Toggle): setup base component

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ToggleBase = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 64px;
+  height: 34px;
+`;
+
+const ToggleSlider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.color.grey};
+  transition: background-color .4s;
+  border-radius: 34px;
+
+  :before {
+    position: absolute;
+    content: "";
+    height: 28px;
+    width: 28px;
+    left: 3px;
+    bottom: 3px;
+    background-color: white;
+    transition: transform .4s;
+    border-radius: 50%;
+  }
+`;
+
+const ToggleInput = styled.input.attrs({
+  type: "checkbox",
+})`
+  opacity: 0;
+  width: 0;
+  height: 0;
+
+  :checked + ${ToggleSlider}:before {
+    transform: translateX(29px);
+  }
+
+  :checked + ${ToggleSlider} {
+    background-color: ${({ theme }) => theme.color.primary};
+  }
+
+  :focus + ${ToggleSlider} {
+    transition: 0;
+    box-shadow: 0 0 0 3px royalblue;
+  }
+`;
 
 const Toggle: React.FC = () => {
   return (
-    <div>Toggle</div>
+    <ToggleBase>
+      <ToggleInput />
+      <ToggleSlider />
+    </ToggleBase>
   );
 };
 
