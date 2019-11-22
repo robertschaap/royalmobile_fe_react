7cf007243d8d5e6e1c47f306e64551a40d14bc4c
robertschaap <robert.schaap@mac.com>
2019-11-22 20:56:56 +0100

    feat(ThemeProviderContainer): extract theme provider to own container

--- src/containers/RootContainer.tsx
+++ src/containers/RootContainer.tsx
@@ -1,39 +1,16 @@
 import React from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import rootReducer from '../reducers';
 import routes from '../constants/routes';
-import theme from '../styles/theme';
 
 import RootLayout from '../components/RootLayout';
 import HomePageContainer from './HomePageContainer';
 import OrderPageContainer from './OrderPageContainer';
 import ProductPageContainer from './ProductPageContainer';
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    font-family: 'Raleway', sans-serif;
-  }
-
-  a:focus,
-  button:focus {
-    outline: none;
-    box-shadow: 0 0 0 3px royalblue;
-  }
-`;
+import ThemeProviderContainer from './ThemeProviderContainer';
 
 const store = createStore(
   rootReducer,
@@ -43,7 +20,7 @@ const store = createStore(
 const Root: React.FC = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
+      <ThemeProviderContainer>
         <BrowserRouter>
           <RootLayout>
             <Route exact path={routes.HOME} component={HomePageContainer} />
@@ -51,8 +28,7 @@ const Root: React.FC = () => {
             <Route path={routes.ORDER} component={OrderPageContainer} />
           </RootLayout>
         </BrowserRouter>
-      </ThemeProvider>
-      <GlobalStyle />
+      </ThemeProviderContainer>
     </Provider>
   );
 };
---
+++ src/containers/ThemeProviderContainer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+
+  a:focus,
+  button:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px royalblue;
+  }
+`;
+
+interface ThemeProviderContainerProps {
+  children: any;
+}
+
+const ThemeProviderContainer: React.FC<ThemeProviderContainerProps> = (props) => {
+  const { children } = props;
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default ThemeProviderContainer;
