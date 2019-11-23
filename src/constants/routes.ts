aa801a02a9d58081ed388be696fc23461c173f04
robertschaap <robert.schaap@mac.com>
2019-11-23 18:08:24 +0100

    feat(routes): add api routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,18 @@
-const routes = {
+const pageRoutes = {
   HOME_PAGE: '/',
-  PRODUCT_PAGE: '/product',
+  PRODUCT_PAGE: '/product/:id',
   ORDER_PAGE: '/order',
 };
 
-export default routes;
+const API = "/api";
+
+const apiRoutes = {
+  API_ROOT: `${API}/`,
+  API_GET_PRODUCTS: `${API}/`,
+};
+
+export default {
+  ...pageRoutes,
+  ...apiRoutes,
+};
+
