5cdfdb0d9cda48c497bc64cd19d05dbecc91499f
robertschaap <robert.schaap@mac.com>
2019-11-23 15:34:46 +0100

    feat(Loader): setup component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoaderBase = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 32px 0;
+`;
+
+const LoaderElement = styled.div`
+  display: inline-block;
+
+  &:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid royalblue;
+    border-color: royalblue transparent royalblue transparent;
+    animation: spin 0.5s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Loader: React.FC = () => {
+  return (
+    <LoaderBase>
+      <LoaderElement />
+    </LoaderBase>
+  );
+};
+
+export default Loader;
