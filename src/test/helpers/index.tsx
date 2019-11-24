c13d9d614094c9fdc7f526229cd9e039013d0696
robertschaap <robert.schaap@mac.com>
2019-11-24 10:35:53 +0100

    test(helpers): setup mock store and initial state

--- src/test/helpers/index.tsx
+++ src/test/helpers/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import theme from '../../styles/theme';
+import { initialState } from '../../reducers';
 
 export const renderWithTheme = (Component: React.ReactElement) => {
   return render(
@@ -13,12 +16,18 @@ export const renderWithTheme = (Component: React.ReactElement) => {
   );
 };
 
-export const renderWithProviders = (Component: React.ReactElement) => {
+const mockStore = configureStore([]);
+
+export const renderWithProviders = (Component: React.ReactElement, customInitialState?: Object) => {
+  const store = mockStore(customInitialState || initialState);
+
   return render(
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        {Component}
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          {Component}
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 };
