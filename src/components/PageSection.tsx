66ca4d171bdfd39dbd40e7304f805d527713b309
robertschaap <robert.schaap@mac.com>
2019-11-24 08:45:41 +0100

    feat(PageSection): setup component

---
+++ src/components/PageSection.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { media_breakpoint_up } from '../styles/theme';
+
+const PageSection = styled.section`
+  margin-bottom: ${({ theme }) => theme.spacing(4)};
+
+  ${media_breakpoint_up.md} {
+    margin-bottom: ${({ theme }) => theme.spacing(8)};
+  }
+`;
+
+export default PageSection;
