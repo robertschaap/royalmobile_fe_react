3a1145a0d6eae2b2d1c75b79df93c9d16e0cae52
robertschaap <robert.schaap@mac.com>
2019-11-24 08:54:43 +0100

    test(useContentCopy): pass actual id to test

--- src/test/hooks/useContentCopy.test.tsx
+++ src/test/hooks/useContentCopy.test.tsx
@@ -2,8 +2,8 @@ import { renderHook } from '@testing-library/react-hooks'
 import { useContentCopy } from '../../hooks/useContentCopy';
 
 it('should render a message when a correct id is passed', () => {
-  const { result } = renderHook(() => useContentCopy("writePageForm.submit"));
-  expect(result.current).toEqual("Submit");
+  const { result } = renderHook(() => useContentCopy("navigation.home"));
+  expect(result.current).toEqual("Home");
 });
 
 it('should render the raw id when an incorrect id is passed', () => {
