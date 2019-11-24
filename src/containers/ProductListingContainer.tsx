bba72b87cae53464c8cf862819235331f71ed385
robertschaap <robert.schaap@mac.com>
2019-11-24 09:23:21 +0100

    feat(ProductListingContainer): add loader and error messages

--- src/containers/ProductListingContainer.tsx
+++ src/containers/ProductListingContainer.tsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getProducts, fetchProductsCollection } from '../ducks/products';
 
 import Button from '../components/Button';
+import ErrorMessage from '../components/ErrorMessage';
+import Loader from '../components/Loader';
+import PageSection from '../components/PageSection';
 import ProductListing from '../components/ProductListing';
 
 const ProductListingContainer: React.FC = () => {
@@ -11,17 +14,29 @@ const ProductListingContainer: React.FC = () => {
 
   useEffect(() => {
     dispatch(fetchProductsCollection());
-  }, []);
+  }, [dispatch]);
+
+  if (productsState.isFetching) {
+    return <Loader />;
+  }
+
+  if (productsState.hasError) {
+    return (
+      <PageSection>
+        <ErrorMessage messageId="common.errorMissing" />
+      </PageSection>
+    );
+  }
 
   return (
-    <>
+    <PageSection>
       <ProductListing
         products={productsState.collection} />
       <Button
         variant="secondary">
         Load More
       </Button>
-    </>
+    </PageSection>
   );
 
 };
