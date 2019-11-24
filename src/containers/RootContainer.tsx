a31ca9ac32edf28ea9404122986f6028487d07d1
robertschaap <robert.schaap@mac.com>
2019-11-24 10:31:45 +0100

    fix(RootContainer): fix error coming from redux devtools

--- src/containers/RootContainer.tsx
+++ src/containers/RootContainer.tsx
@@ -20,7 +20,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(sagaMiddleware),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+    ...((window as any).__REDUX_DEVTOOLS_EXTENSION__ ? [(window as any).__REDUX_DEVTOOLS_EXTENSION__()] : []),
   ),
 );
 
