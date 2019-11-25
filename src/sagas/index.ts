38bf98f7c52cfb1f15af4554c015d65fedc1f493
robertschaap <robert.schaap@mac.com>
2019-11-25 20:43:27 +0100

    feat(rootSaga): add subscriptionsSaga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 import products from './products';
+import subscriptions from './subscriptions';
 
 function* rootSaga() {
   yield all([
     ...products,
+    ...subscriptions,
   ]);
 }
 
