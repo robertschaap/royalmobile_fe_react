4ab1d8864717758da4f673e06699064abadea66a
robertschaap <robert.schaap@mac.com>
2019-11-26 20:16:23 +0100

    feat(products): type actions

--- src/ducks/products.ts
+++ src/ducks/products.ts
@@ -1,22 +1,42 @@
 import { StoreState } from '../reducers/index'
+import { Product } from '../types/products';
 
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 export const FETCH_PRODUCTS_ERROR = 'FETCH_PRODUCTS_ERROR';
 
-export const fetchProducts = () => ({
+interface FetchProductsAction {
+  type: typeof FETCH_PRODUCTS;
+}
+
+export const fetchProducts = (): FetchProductsAction => ({
   type: FETCH_PRODUCTS,
 });
 
-export const fetchProductsSuccess = (payload: any) => ({
+interface FetchProductsSucesssAction {
+  type: typeof FETCH_PRODUCTS_SUCCESS;
+  payload: Product[];
+}
+
+export const fetchProductsSuccess = (payload: Product[]): FetchProductsSucesssAction => ({
   type: FETCH_PRODUCTS_SUCCESS,
   payload,
 });
 
-export const fetchProductsError = (error: any) => ({
+interface FetchProductsErrorAction {
+  type: typeof FETCH_PRODUCTS_ERROR;
+  error: string;
+}
+
+export const fetchProductsError = (error: string): FetchProductsErrorAction => ({
   type: FETCH_PRODUCTS_ERROR,
   error,
 });
+
+export type ProductsActiontypes =
+  | FetchProductsAction
+  | FetchProductsSucesssAction
+  | FetchProductsErrorAction
 
 export const getProducts = (state: StoreState) => state.products;
 export const getProductsCollection = (state: StoreState) => state.products.collection;
