c4f2e781d634ce6931eac938f207263175bad148
robertschaap <robert.schaap@mac.com>
2019-11-26 19:28:56 +0100

    test(NavigationLinks) add tests

--- src/components/NavigationLinks.tsx
+++ src/components/NavigationLinks.tsx
@@ -4,24 +4,32 @@ import routes from '../constants/routes';
 import { useContentCopy } from '../hooks/useContentCopy';
 
 export const ProductsForYouLink: React.FC = () => (
-  <Link to={routes.CONSUMER_PAGE}>
+  <Link
+    data-testid="products-for-you-link"
+    to={routes.CONSUMER_PAGE}>
     {useContentCopy("navigation.consumer")}
   </Link>
 );
 
 export const ForYourBusinessLink: React.FC = () => (
-  <Link to={routes.BUSINESS_PAGE}>
+  <Link
+    data-testid="for-your-business-link"
+    to={routes.BUSINESS_PAGE}>
     {useContentCopy("navigation.business")}
   </Link>
 );
 
 export const MyAccountLink: React.FC = () => (
-  <Link to={routes.MY_ACCOUNT_PAGE}>
+  <Link
+    data-testid="my-account-link"
+    to={routes.MY_ACCOUNT_PAGE}>
     {useContentCopy("navigation.myAccount")}
   </Link>
 );
 export const HelpLink: React.FC = () => (
-  <Link to={routes.HELP_PAGE}>
+  <Link
+    data-testid="help-link"
+    to={routes.HELP_PAGE}>
     {useContentCopy("navigation.help")}
   </Link>
 );
---
+++ src/test/components/NavigationLinks.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  ForYourBusinessLink,
+  HelpLink,
+  MyAccountLink,
+  ProductsForYouLink,
+} from '../../components/NavigationLinks';
+
+import { renderWithProviders } from '../helpers';
+import { fireEvent, getByTestId } from '@testing-library/dom';
+
+describe('<NavigationLinks />', () => {
+  describe('<ForYourBusinessLink />', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(<ForYourBusinessLink />);
+    });
+
+    it('should redirect the user when clicked on', () => {
+      const { getByTestId } = renderWithProviders(<ForYourBusinessLink />);
+
+      fireEvent.click(getByTestId('for-your-business-link'));
+
+      expect(location.pathname).toContain('/business');
+    });
+  });
+
+  describe('<HelpLink />', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(<HelpLink />);
+    });
+
+    it('should redirect the user when clicked on', () => {
+      const { getByTestId } = renderWithProviders(<HelpLink />);
+
+      fireEvent.click(getByTestId('help-link'));
+
+      expect(location.pathname).toContain('/help');
+    });
+  });
+
+  describe('<MyAccountLink />', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(<MyAccountLink />);
+    });
+
+    it('should redirect the user when clicked on', () => {
+      const { getByTestId } = renderWithProviders(<MyAccountLink />);
+
+      fireEvent.click(getByTestId('my-account-link'));
+
+      expect(location.pathname).toContain('/myroyalmobile');
+    });
+  });
+
+  describe('<ProductsForYouLink />', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(<ProductsForYouLink />);
+    });
+
+    it('should redirect the user when clicked on', () => {
+      const { getByTestId } = renderWithProviders(<ProductsForYouLink />);
+
+      fireEvent.click(getByTestId('products-for-you-link'));
+
+      expect(location.pathname).toContain('/consumer');
+    });
+  });
+});
