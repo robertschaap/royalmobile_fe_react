c9381bee9bdf8b32f453c34859d234684b21bab7
robertschaap <robert.schaap@mac.com>
2019-11-29 23:19:00 +0100

    feature(OrderPageContainer): add Orderpage

--- src/containers/OrderPageContainer.tsx
+++ src/containers/OrderPageContainer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import OrderPage from '../components/OrderPage';
 import PageTitle from '../components/PageTitle';
 
 const OrderPageContainer: React.FC = () => {
   return (
     <>
       <PageTitle page="Order" />
-      <div>OrderPageContainer</div>
+      <OrderPage />
     </>
   );
 };
