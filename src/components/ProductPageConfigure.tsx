c0d133e797b2bb3125b620d2beb52ec37bff7a22
robertschaap <robert.schaap@mac.com>
2019-12-02 19:26:18 +0100

    feat(ProductPageConfigure): add PageSection

--- src/components/ProductPageConfigure.tsx
+++ src/components/ProductPageConfigure.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
-import { media_breakpoint_up } from '../styles/theme';
 
 import SectionHeader from './SectionHeader';
 import DurationListing from './DurationListing';
 import SubscriptionListing from './SubscriptionListing';
 import PaymentSelector from './PaymentSelector';
-
-const ProductPageConfigureBase = styled.section`
-  margin-bottom: ${({ theme }) => theme.spacing(4)};
-
-  ${media_breakpoint_up.md} {
-    margin-bottom: ${({ theme }) => theme.spacing(8)};
-  }
-`;
+import PageSection from './PageSection';
 
 const ProductPageConfigure: React.FC = () => {
   return (
-    <ProductPageConfigureBase>
+    <PageSection>
       <SectionHeader>Configure your plan</SectionHeader>
       <DurationListing />
       <SubscriptionListing />
       <PaymentSelector />
-    </ProductPageConfigureBase>
+    </PageSection>
   );
 };
 
