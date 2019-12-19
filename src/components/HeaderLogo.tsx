7eb38876c2c812fbcf2818990c9bae356491f17b
robertschaap <robert.schaap@mac.com>
2019-12-19 20:56:04 +0100

    refactor(Headerlogo): replace old breakpoint syntax

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { breakpoint_up } from '../styles/theme';
+import { media_breakpoint_up } from '../styles/theme';
 import routes from '../constants/routes';
 
 import Link from './Link';
@@ -8,7 +8,7 @@ import Link from './Link';
 const HeaderLogoBase = styled.span`
   font-size: 40px;
 
-  @media ${breakpoint_up.md} {
+  ${media_breakpoint_up.md} {
     font-size: 48px;
   }
 `;
