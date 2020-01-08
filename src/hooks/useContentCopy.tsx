8777e3807f19174f82e988cbcf5659367e4d148f
robertschaap <robert.schaap@mac.com>
2020-01-08 22:39:23 +0100

    refactor(useContentCopy): extracted content object to own file

--- src/hooks/useContentCopy.tsx
+++ src/hooks/useContentCopy.tsx
@@ -1,39 +1,5 @@
 import { createContext, useContext } from 'react';
-
-interface ContentCopy {
-  [key: string]: string;
-}
-
-const contentCopy: ContentCopy = Object.freeze({
-  'common.errorMissing': 'Something should\'ve been here, but it isn\'t.',
-  'common.loadMore': 'Load More',
-  'common.mobile': 'Mobile',
-  'common.royal': 'Royal',
-  'common.royalMobile': 'RoyalMobile',
-
-  'navigation.business': 'For Your Business',
-  'navigation.consumer': 'Products For You',
-  'navigation.contact': 'Contact',
-  'navigation.help': 'Help',
-  'navigation.home': 'Home',
-  'navigation.myAccount': 'My Account',
-  'navigation.ourProducts': 'Our Products',
-
-  'benefits.get': 'GET',
-  'benefits.threeMonthsFree': 'Three months free inside of your regular bundle when you refer a friend',
-  'benefits.doubleData': 'Double data in your bundle plus a 5 euro discount when you combine with Royal Internet',
-
-  'order.readyToOrder': 'I\'m ready to order!',
-  'order.renewContract': 'I\'m ready to renew my contract!',
-
-  'product.configurePlan': 'Configure your plan',
-  'product.customiseDevice': 'Customise your device',
-  'product.summingUp': 'Summing it  all up',
-
-  'usp.freeShipping': 'Free Shipping on All Orders',
-  'usp.noConnectionFeeAcquisition': 'No Connection Fee For New Customers',
-  'usp.noFalsePromises': 'Guaranteed no False Promises',
-});
+import contentCopy, { ContentCopy } from '../constants/contentCopy';
 
 const ContentContext = createContext<ContentCopy>(contentCopy);
 
