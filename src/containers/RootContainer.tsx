5a2031d00c168835eba1cfa42dee32b90a28b095
robertschaap <robert.schaap@mac.com>
2020-01-09 21:28:28 +0100

    feat(RootContainer): add switch and ErrorPageContainer

--- src/containers/RootContainer.tsx
+++ src/containers/RootContainer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import routes from '../constants/routes';
 
 import CampaignsPageContainer from './CampaignsPageContainer';
 import ContactPageContainer from './ContactPageContainer';
+import ErrorPageContainer from './ErrorPageContainer';
 import HelpPageContainer from './HelpPageContainer';
 import HomePageContainer from './HomePageContainer';
 import OrderPageContainer from './OrderPageContainer';
@@ -22,13 +23,16 @@ const Root: React.FC = () => {
       <ThemeProviderContainer>
         <BrowserRouter>
           <RootLayout>
-            <Route exact path={routes.HOME_PAGE} component={HomePageContainer} />
-            <Route path={routes.PRODUCT_PAGE} component={ProductPageContainer} />
-            <Route path={routes.ORDER_PAGE} component={OrderPageContainer} />
+            <Switch>
+              <Route exact path={routes.HOME_PAGE} component={HomePageContainer} />
+              <Route path={routes.PRODUCT_PAGE} component={ProductPageContainer} />
+              <Route path={routes.ORDER_PAGE} component={OrderPageContainer} />
 
-            <Route path={routes.CAMPAIGNS_PAGE} component={CampaignsPageContainer} />
-            <Route path={routes.CONTACT_PAGE} component={ContactPageContainer} />
-            <Route path={routes.HELP_PAGE} component={HelpPageContainer} />
+              <Route path={routes.CAMPAIGNS_PAGE} component={CampaignsPageContainer} />
+              <Route path={routes.CONTACT_PAGE} component={ContactPageContainer} />
+              <Route path={routes.HELP_PAGE} component={HelpPageContainer} />
+              <Route component={ErrorPageContainer} />
+            </Switch>
           </RootLayout>
         </BrowserRouter>
       </ThemeProviderContainer>
