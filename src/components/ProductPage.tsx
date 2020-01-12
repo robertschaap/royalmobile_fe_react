f5cf9fcceb227db1ce5af04003c819fd44ca869b
robertschaap <robert.schaap@mac.com>
2020-01-12 17:20:42 +0100

    feat(ProductPage): wrap buttons in Grid

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -7,6 +7,7 @@ import ProductPageConfigure from './ProductPageConfigure';
 import ProductPageCustomise from './ProductPageCustomise';
 import PageSection from './PageSection';
 import { useContentCopy } from '../hooks';
+import { GridBase, GridItem } from './Grid';
 
 const OrderButton = styled(Button)`
   margin-bottom: ${({ theme }) => theme.spacing(3)};
@@ -19,14 +20,18 @@ const ProductPage: React.FC = () => {
       <ProductPageConfigure />
       <ProductPageSummary />
       <PageSection>
-        <OrderButton
-          variant="primary">
-          {useContentCopy('order.readyToOrder')}
-        </OrderButton>
-        <Button
-          variant="secondary">
-          {useContentCopy('order.renewContract')}
-        </Button>
+        <GridBase>
+          <GridItem lg={2}>
+            <OrderButton
+              variant="primary">
+              {useContentCopy('order.readyToOrder')}
+            </OrderButton>
+            <Button
+              variant="secondary">
+              {useContentCopy('order.renewContract')}
+            </Button>
+          </GridItem>
+        </GridBase>
       </PageSection>
     </>
   );
