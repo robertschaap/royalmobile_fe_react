370098cf30dc2edff1ce260c85c493f79f878318
robertschaap <robert.schaap@mac.com>
2020-01-15 22:32:53 +0100

    feat(Hidden): setup component

---
+++ src/components/Hidden.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { media_breakpoint_up } from '../styles/theme';
+
+interface HiddenProps {
+  sm?: boolean;
+  md?: boolean;
+  lg?: boolean;
+}
+
+const Hidden = styled.div<HiddenProps>`
+  ${({ sm }) => (sm ? 'display: none;' : '')}
+
+  ${media_breakpoint_up.md} {
+    ${({ md }) => (md ? 'display: none;' : 'display: unset')}
+  }
+
+  ${media_breakpoint_up.lg} {
+    ${({ lg }) => (lg ? 'display: none;' : 'display: unset')}
+  }
+`;
+
+export default Hidden;
