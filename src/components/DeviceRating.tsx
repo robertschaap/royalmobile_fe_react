eec88b259984114b27170213a10ed5c1802e2d81
robertschaap <robert.schaap@mac.com>
2020-01-19 13:10:48 +0100

    feat(DeviceRating): add some margin

--- src/components/DeviceRating.tsx
+++ src/components/DeviceRating.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { media_breakpoint_up } from '../styles/theme';
 
 const DeviceRatingBase = styled.div`
   margin-bottom: ${({ theme }) => theme.spacing(2)};
+
+  ${media_breakpoint_up.md} {
+    margin-bottom: ${({ theme }) => theme.spacing(4)};
+  }
 `;
 
 const StarFilled = styled.span`
