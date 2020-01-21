c90a91eef2b091563e879f390f5542863bd51e44
robertschaap <robert.schaap@mac.com>
2020-01-21 20:35:02 +0100

    feat(ContactPage): setup page

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { GridBase, GridItem } from './Grid';
+import IconFacebook from './IconFacebook';
+import IconLinkedIn from './IconLinkedIn';
+import IconTwitter from './IconTwitter';
 import PageSection from './PageSection';
 import SectionHeader from './SectionHeader';
-import { GridBase, GridItem } from './Grid';
 
 const ContactInfo = styled(GridItem)`
   padding-left: ${({ theme }) => theme.spacing(4)};
+`;
+
+const SocialIcons = styled.ul`
+  display: flex;
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+`;
+
+const SocialIcon = styled.li`
+  width: 32px;
+  height: 32px;
+  padding: ${({ theme }) => theme.spacing(2)};
+  background-color: ${({ theme }) => theme.color.primary};
+  border-radius: 8px;
+  margin-right: ${({ theme }) => theme.spacing(2)};
+  display: flex;
+
+  > svg {
+    width: 100%;
+  }
 `;
 
 const ContactPage = () => {
@@ -16,6 +40,17 @@ const ContactPage = () => {
       <GridBase>
         <ContactInfo md={2} lg={2}>
           <p>We can’t have you contacting us directly because phone lines cost money to operate, but feel free to drop by in one of our stores or reach out to us on social media.</p>
+          <SocialIcons>
+            <SocialIcon>
+              <IconTwitter />
+            </SocialIcon>
+            <SocialIcon>
+              <IconFacebook />
+            </SocialIcon>
+            <SocialIcon>
+              <IconLinkedIn />
+            </SocialIcon>
+          </SocialIcons>
         </ContactInfo>
       </GridBase>
     </PageSection>
