47db422bcbd30ac4a60364a68b1627f8f22c3c9e
robertschaap <robert.schaap@mac.com>
2020-01-22 19:42:26 +0100

    feat(RootContainer): add code splitting of components

--- src/containers/RootContainer.tsx
+++ src/containers/RootContainer.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import routes from '../constants/routes';
 
-import CampaignsPageContainer from './CampaignsPageContainer';
-import ContactPageContainer from './ContactPageContainer';
-import ErrorPageContainer from './ErrorPageContainer';
-import HelpPageContainer from './HelpPageContainer';
-import HomePageContainer from './HomePageContainer';
-import OrderPageContainer from './OrderPageContainer';
-import ProductPageContainer from './ProductPageContainer';
 import RootLayout from '../components/RootLayout';
 
 import ThemeProviderContainer from './ThemeProviderContainer';
@@ -17,22 +10,31 @@ import StoreProviderContainer from './StoreProviderContainer';
 
 import '../styles/normalize.css';
 
+const CampaignsPageContainer = React.lazy(() => import('./CampaignsPageContainer'));
+const ContactPageContainer = React.lazy(() => import('./ContactPageContainer'));
+const ErrorPageContainer = React.lazy(() => import('./ErrorPageContainer'));
+const HelpPageContainer = React.lazy(() => import('./HelpPageContainer'));
+const HomePageContainer = React.lazy(() => import('./HomePageContainer'));
+const OrderPageContainer = React.lazy(() => import('./OrderPageContainer'));
+const ProductPageContainer = React.lazy(() => import('./ProductPageContainer'));
+
 const Root: React.FC = () => {
   return (
     <StoreProviderContainer>
       <ThemeProviderContainer>
         <BrowserRouter>
           <RootLayout>
-            <Switch>
-              <Route exact path={routes.HOME_PAGE} component={HomePageContainer} />
-              <Route path={routes.PRODUCT_PAGE} component={ProductPageContainer} />
-              <Route path={routes.ORDER_PAGE} component={OrderPageContainer} />
-
-              <Route path={routes.CAMPAIGNS_PAGE} component={CampaignsPageContainer} />
-              <Route path={routes.CONTACT_PAGE} component={ContactPageContainer} />
-              <Route path={routes.HELP_PAGE} component={HelpPageContainer} />
-              <Route component={ErrorPageContainer} />
-            </Switch>
+            <Suspense fallback={null}>
+              <Switch>
+                <Route exact path={routes.HOME_PAGE} component={HomePageContainer} />
+                <Route path={routes.PRODUCT_PAGE} component={ProductPageContainer} />
+                <Route path={routes.ORDER_PAGE} component={OrderPageContainer} />
+                <Route path={routes.CAMPAIGNS_PAGE} component={CampaignsPageContainer} />
+                <Route path={routes.CONTACT_PAGE} component={ContactPageContainer} />
+                <Route path={routes.HELP_PAGE} component={HelpPageContainer} />
+                <Route component={ErrorPageContainer} />
+              </Switch>
+            </Suspense>
           </RootLayout>
         </BrowserRouter>
       </ThemeProviderContainer>
