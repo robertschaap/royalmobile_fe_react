221c1ce0df5a37a50a4ea26fe28da1a37e2281c4
robertschaap <robert.schaap@mac.com>
2020-02-09 09:29:20 +0100

    chore(package): add stubs command to package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootContainer from './containers/RootContainer';
+import { makeStubsServer } from './stubs';
+
+// TODO: make import conditional so it doesn't pollute the production package
+if (process.env.REACT_APP_USESTUBS === 'true') {
+  makeStubsServer();
+}
 
 ReactDOM.render(
   <RootContainer />,
