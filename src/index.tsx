5ee5c3914c71d073fd7fe1bb21f69a17956a8ec2
robertschaap <robert.schaap@mac.com>
2020-02-16 12:18:58 +0100

    feat(index): add conditional import of stubs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootContainer from './containers/RootContainer';
-import { makeStubsServer } from './stubs';
 
-// TODO: make import conditional so it doesn't pollute the production package
-if (process.env.REACT_APP_USESTUBS === 'true') {
-  makeStubsServer();
-}
+(async () => {
+  if (process.env.REACT_APP_USESTUBS === 'true') {
+    const { default: setupStubsServer } = await import('./stubs');
+    await setupStubsServer();
+  }
 
-ReactDOM.render(
-  <RootContainer />,
-  document.getElementById('root'),
-);
+  ReactDOM.render(
+    <RootContainer />,
+    document.getElementById('root'),
+  );
+})();
