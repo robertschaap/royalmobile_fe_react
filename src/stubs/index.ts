6149d93158c091e0e067e4377d84757772511792
robertschaap <robert.schaap@mac.com>
2020-02-16 12:18:42 +0100

    feat(stubs): make stubs setup default export

--- src/stubs/index.ts
+++ src/stubs/index.ts
@@ -2,11 +2,10 @@ import { getProducts, getSubscriptions } from './apiResponses';
 import { Server } from 'miragejs';
 import routes from '../constants/routes';
 
-export const makeStubsServer = () => new Server({
+export default () => new Server({
   environment: 'development',
   routes() {
     this.get(routes.API_GET_PRODUCTS, getProducts);
     this.get(routes.API_GET_SUBSCRIPTIONS, getSubscriptions);
   },
 });
-
