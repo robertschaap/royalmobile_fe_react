d84b5d1934c5bf96859097073e06fb0f18f9d680
robertschaap <robert.schaap@mac.com>
2020-02-16 19:05:22 +0100

    feat(products): setup ProductVariant type

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,21 +1,23 @@
 /* eslint-disable import/prefer-default-export */
+export interface ProductVariant {
+  id: number;
+  variantId: string;
+  color: string;
+  capacity: string;
+  is_in_stock: boolean;
+  is_preorder: boolean;
+  regular_price: string;
+  discounted_price: string;
+  has_discounts: boolean;
+}
+
 export interface Product {
   id: number;
   manufacturer: string;
   model: string;
   modelId: string;
-  variants: Array<{
-    id: number;
-    variantId: string;
-    color: string;
-    capacity: string;
-    is_in_stock: boolean;
-    is_preorder: boolean;
-    regular_price: string;
-    discounted_price: string;
-    has_discounts: boolean;
-  }>;
-  specifications?: {},
+  variants: ProductVariant[];
+  specifications?: {};
 }
 
 export const productsStub: Product[] = [{
