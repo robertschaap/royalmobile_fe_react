e38c88cae976c15927d9d0b4110458ea14622d13
robertschaap <robert.schaap@mac.com>
2020-02-16 12:55:59 +0100

    feat(subscriptions): setup factory for stubs

--- src/stubs/subscriptions.ts
+++ src/stubs/subscriptions.ts
@@ -1,22 +1,37 @@
 import { Subscription } from '../types/subscriptions';
 
-const subscription: Subscription = {
-  id: 1,
-  subscriptionId: 'royalmobile-10gb-2year',
-  duration: '2_year',
-  data: '10gb',
-  benefits_long: [
-    'unlimited calls',
-    'unlimited texts',
-    'unlimited roaming',
-  ],
-  benefits_short: 'unlimited<br/> calls, text, roaming',
-  regular_price: '20',
-};
+interface CreateSubscripton {
+  id: number;
+  data: string;
+  duration: string;
+  price: string;
+}
+const createSubscription = (props: CreateSubscripton): Subscription => {
+  const { id, data, duration, price } = props;
+
+  return {
+    id: id,
+    subscriptionId: `royalmobile-${data}-${duration}year`,
+    duration: `${duration}_year`,
+    data: data,
+    benefits_long: [
+      'unlimited calls',
+      'unlimited texts',
+      'unlimited roaming',
+    ],
+    benefits_short: 'unlimited<br/> calls, text, roaming',
+    regular_price: price,
+  };
+}
 
 export const subscriptions: Subscription[] = [
-  { ...subscription, id: 1 },
-  { ...subscription, id: 2 },
-  { ...subscription, id: 3 },
-  { ...subscription, id: 4 },
+  createSubscription({ id: 1, data: '20gb', duration: '1', price: '20' }),
+  createSubscription({ id: 2, data: '20gb', duration: '1', price: '20' }),
+  createSubscription({ id: 3, data: '20gb', duration: '1', price: '20' }),
+  createSubscription({ id: 4, data: '20gb', duration: '1', price: '20' }),
+
+  createSubscription({ id: 5, data: '20gb', duration: '2', price: '20' }),
+  createSubscription({ id: 6, data: '20gb', duration: '2', price: '20' }),
+  createSubscription({ id: 7, data: '20gb', duration: '2', price: '20' }),
+  createSubscription({ id: 8, data: '20gb', duration: '2', price: '20' }),
 ];
