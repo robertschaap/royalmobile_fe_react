14feaf0475a7046680219898f087e7e3dc3a81f4
robertschaap <robert.schaap@mac.com>
2020-02-16 19:26:18 +0100

    feat(products): add createProductVariant function

--- src/stubs/products.ts
+++ src/stubs/products.ts
@@ -1,31 +1,51 @@
-import { Product } from '../types/products';
+import { Product, ProductVariant } from '../types/products';
 
 const defaultModelId = 'apple-iphone-x';
+
+interface CreateProductVariant {
+  id: number
+  modelId?: string;
+  capacity: string;
+  color: string;
+}
+
+const createProductVariant = (props: CreateProductVariant) => {
+  const { id, modelId = defaultModelId, capacity, color } = props;
+
+  return {
+    id: id,
+    variantId: `${modelId}-${capacity}-${color}`,
+    color: color,
+    capacity: capacity,
+    is_in_stock: true,
+    is_preorder: true,
+    regular_price: '265',
+    discounted_price: '239',
+    has_discounts: true,
+  };
+};
 
 interface CreateProduct {
   id: number;
   modelId?: string;
+  variants?: ProductVariant[],
 }
 
 const createProduct = (props: CreateProduct): Product => {
-  const { id, modelId = defaultModelId } = props;
+  const { id, modelId = defaultModelId, variants = [] } = props;
 
   return {
     id: id,
     manufacturer: 'Apple',
     model: 'iPhone X',
     modelId: modelId,
-    variants: [{
-      id: 1,
-      variantId: `${modelId}-16gb-hotpink`,
-      color: 'hotpink',
-      capacity: '16gb',
-      is_in_stock: true,
-      is_preorder: true,
-      regular_price: '265',
-      discounted_price: '239',
-      has_discounts: true,
-    }],
+    variants: [
+      createProductVariant({
+        id: 1,
+        capacity: '16gb',
+        color: 'hotpink',
+      }),
+    ],
   };
 }
 
