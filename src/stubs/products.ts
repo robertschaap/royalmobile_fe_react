115f08f1f314221eb0b814e37f43f41237dfce17
robertschaap <robert.schaap@mac.com>
2020-02-16 12:56:38 +0100

    feat(products): setup factory for products

--- src/stubs/products.ts
+++ src/stubs/products.ts
@@ -1,28 +1,37 @@
 import { Product } from '../types/products';
 
-const product: Product = {
-  id: 1,
-  manufacturer: 'Apple',
-  model: 'iPhone X',
-  modelId: 'apple-iphone-x',
-  variants: [{
-    id: 1,
-    variantId: 'apple-iphone-x-16gb-hotpink',
-    color: 'hotpink',
-    capacity: '16gb',
-    is_in_stock: true,
-    is_preorder: true,
-    regular_price: '265',
-    discounted_price: '239',
-    has_discounts: true,
-  }],
-};
+interface CreateProduct {
+  id: number;
+  modelId?: string;
+}
+
+const createProduct = (props: CreateProduct): Product => {
+  const { id, modelId = 'apple-iphone-x' } = props;
+
+  return {
+    id: id,
+    manufacturer: 'Apple',
+    model: 'iPhone X',
+    modelId: modelId,
+    variants: [{
+      id: 1,
+      variantId: `${modelId}-16gb-hotpink`,
+      color: 'hotpink',
+      capacity: '16gb',
+      is_in_stock: true,
+      is_preorder: true,
+      regular_price: '265',
+      discounted_price: '239',
+      has_discounts: true,
+    }],
+  };
+}
 
 export const products: Product[] = [
-  { ...product, id: 1 },
-  { ...product, id: 2 },
-  { ...product, id: 3 },
-  { ...product, id: 4 },
-  { ...product, id: 5 },
-  { ...product, id: 6 },
+  createProduct({ id: 1 }),
+  createProduct({ id: 2 }),
+  createProduct({ id: 3 }),
+  createProduct({ id: 4 }),
+  createProduct({ id: 5 }),
+  createProduct({ id: 6 }),
 ];
