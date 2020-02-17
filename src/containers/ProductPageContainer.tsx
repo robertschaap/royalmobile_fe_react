24ff9f22f3514298c187d342260a6abef89a72d5
robertschaap <robert.schaap@mac.com>
2020-02-17 11:42:00 +0100

    feat(ProductPageContainer): add components and loading logic

--- src/containers/ProductPageContainer.tsx
+++ src/containers/ProductPageContainer.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectProducts, fetchProducts } from '../ducks/products';
+import { selectSubscriptions, fetchSubscriptions } from '../ducks/subscriptions';
 
+import ErrorMessage from '../components/ErrorMessage';
 import PageTitle from '../components/PageTitle';
-import ProductPage from '../components/ProductPage';
+import ProductPageConfigure from '../components/ProductPageConfigure';
+import ProductPageCustomise from '../components/ProductPageCustomise';
+import ProductPageOrder from '../components/ProductPageOrder';
+import ProductPageSummary from '../components/ProductPageSummary';
+import PageSection from '../components/PageSection';
+import Loader from '../components/Loader';
 
 const ProductPageContainer: React.FC = () => {
+  const dispatch = useDispatch();
+  const productsState = useSelector(selectProducts);
+  const subscriptionsState = useSelector(selectSubscriptions);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+    dispatch(fetchSubscriptions());
+  }, [dispatch]);
+
+  if (productsState.hasError || subscriptionsState.hasError) {
+    return (
+      <>
+        <PageTitle page="Product" />
+        <PageSection>
+          <ErrorMessage messageId="common.errorMissing" />
+        </PageSection>
+      </>
+    );
+  }
+
+  if (productsState.isFetching || subscriptionsState.isFetching) {
+    return (
+      <>
+        <PageTitle page="Product" />
+        <PageSection>
+          <Loader show />
+        </PageSection>
+      </>
+    );
+  }
+
   return (
     <>
       <PageTitle page="Product" />
-      <ProductPage />
+      <ProductPageCustomise />
+      <ProductPageConfigure />
+      <ProductPageSummary />
+      <ProductPageOrder />
     </>
   );
 };
