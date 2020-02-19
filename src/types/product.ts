2fa6b1aef4b063585e118c6026ef0147e9544da3
robertschaap <robert.schaap@mac.com>
2020-02-19 19:52:31 +0100

    feat(product): setup type

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+import { ProductVariant } from "./products";
+
+// TODO: complete this with product details information
+interface Product {
+  id: number;
+  manufacturer: string;
+  model: string;
+  modelId: string;
+  variants: ProductVariant[];
+  specifications?: {};
+}
