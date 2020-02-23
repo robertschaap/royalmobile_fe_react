1b942f39adf0b31400cb35e739c4fd0724ff480f
robertschaap <robert.schaap@mac.com>
2020-02-23 18:02:39 +0100

    feat(reducers): add product reducer to rootreducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,14 +1,17 @@
 import { combineReducers } from 'redux';
 
+import { productReducer, initialProductState } from './product';
 import { productsReducer, initialProductsState } from './products';
 import { subscriptionsReducer, initialSubscriptionsState } from './subscriptions';
 
 export const initialState = {
+  product: initialProductState,
   products: initialProductsState,
   subscriptions: initialSubscriptionsState,
 };
 
 export const rootReducer = () => combineReducers({
+  product: productReducer,
   products: productsReducer,
   subscriptions: subscriptionsReducer,
 });
