a20e9fe4f2af2d8129db6820a0c2218c771b5232
robertschaap <robert.schaap@mac.com>
2020-02-24 20:30:03 +0100

    feat(store): draft useStore hook

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { selectProducts, fetchProducts } from '../ducks/products';
+
+const ProductsStore = {
+  selectProducts,
+  fetchProducts,
+}
+
+/**
+ * TODO: finalise
+ * The idea is to have a hook which can compose a store, the only thing a local component
+ * would need to know is what store it is calling, the usage of select/dispatch should
+ * be abstracted away from the component
+ */
+const useStore = () => {
+  const dispatch = useDispatch();
+
+  return {
+    select: useSelector,
+    dispatch: dispatch,
+  }
+}
 
 export {
   useDispatch,
   useSelector,
+
+  useStore,
+  ProductsStore,
 };
