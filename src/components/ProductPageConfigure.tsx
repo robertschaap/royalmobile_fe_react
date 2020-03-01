8cf292f76d80bc0625787effe02683b95a7635a0
robertschaap <robert.schaap@mac.com>
2020-03-01 19:59:54 +0100

    feat(ProductPageConfigure): add state logic for DurationListing

--- src/components/ProductPageConfigure.tsx
+++ src/components/ProductPageConfigure.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SectionHeader from './SectionHeader';
 import DurationListing from './DurationListing';
@@ -8,10 +8,20 @@ import PageSection from './PageSection';
 import { useContentCopy } from '../hooks';
 
 const ProductPageConfigure: React.FC = () => {
+  const durations = [
+    { id: 'a', duration: '2 year contract' },
+    { id: 'b', duration: '1 year contract' },
+  ];
+
+  const [selectedDurationId, setSelectedDurationId] = useState(durations[0].id);
+
   return (
     <PageSection>
       <SectionHeader>{useContentCopy('product.configurePlan')}</SectionHeader>
-      <DurationListing />
+      <DurationListing
+        onClickDuration={(id) => setSelectedDurationId(id)}
+        durations={durations}
+        selectedDurationId={selectedDurationId}/>
       <SubscriptionListing />
       <PaymentSelector />
     </PageSection>
