809e917c6fde85055bdda8cd25dad3151b701267
robertschaap <robert.schaap@mac.com>
2020-03-03 20:00:38 +0100

    test(SubscriptionListing): test list of subscriptions

--- src/test/components/SubscriptionListing.test.tsx
+++ src/test/components/SubscriptionListing.test.tsx
@@ -3,6 +3,12 @@ import SubscriptionListing from '../../components/SubscriptionListing';
 
 import { renderWithProviders } from '../helpers';
 
+const subscriptions = [
+  { id: 'subscription-id1' },
+  { id: 'subscription-id2' },
+  { id: 'subscription-id3' },
+];
+
 describe('<SubscriptionListing />', () => {
   it('should render without crashing', () => {
     const { getByTestId } = renderWithProviders(
@@ -14,9 +20,19 @@ describe('<SubscriptionListing />', () => {
 
     expect(getByTestId('subscription-listing').children.length).toBe(0);
   });
+
+  // TODO: add rendered text to this when implemented
+  it('should render a list of subscriptions', () => {
+    const { getByTestId } = renderWithProviders(
+      <SubscriptionListing
+        onClickSubscription={jest.fn()}
+        selectedSubscriptionId={subscriptions[0].id}
+        subscriptions={subscriptions} />
+    );
+
+    expect(getByTestId('subscription-listing').children.length).toBe(3);
+  });
 });
 
-// renders a list of subscriptions
-// or not
 // has a default selectede
 // has an onclick handler
