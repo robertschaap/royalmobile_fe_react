e0be5c44a0f3f326f0183fe56f8ed8a00b07f35c
robertschaap <robert.schaap@mac.com>
2020-03-05 20:31:24 +0100

    test(DeviceCapacityListing): add test for rendering list of capacities

--- src/test/components/DeviceCapacityListing.test.tsx
+++ src/test/components/DeviceCapacityListing.test.tsx
@@ -3,6 +3,12 @@ import DeviceCapacityListing from '../../components/DeviceCapacityListing';
 
 import { renderWithProviders } from '../helpers';
 
+const capacities = [
+  { id: 'capacity-id1', capacity: 'Capacity 1' },
+  { id: 'capacity-id2', capacity: 'Capacity 2' },
+  { id: 'capacity-id3', capacity: 'Capacity 3' },
+];
+
 describe('<DeviceCapacityListing />', () => {
   it('should render without crashing', () => {
     const { getByTestId } = renderWithProviders(
@@ -14,5 +20,17 @@ describe('<DeviceCapacityListing />', () => {
     );
 
     expect(getByTestId('device-capacity-listing').children.length).toBe(0);
+  });
+
+  it('should render a list of device capacities', () => {
+    const { getByTestId } = renderWithProviders(
+      <DeviceCapacityListing
+        capacities={capacities}
+        onClickCapacity={jest.fn()}
+        selectedCapacityId=""
+      />
+    );
+
+    expect(getByTestId('device-capacity-listing').children.length).toBe(3);
   });
 });
