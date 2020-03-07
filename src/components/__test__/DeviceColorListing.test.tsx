52448ed5f9c718c842c8ebe37813837e4b7418d9
robertschaap <robert.schaap@mac.com>
2020-03-07 13:03:49 +0100

    test(DeviceColorListing): add missing props

--- src/components/__test__/DeviceColorListing.test.tsx
+++ src/components/__test__/DeviceColorListing.test.tsx
@@ -3,16 +3,31 @@ import DeviceColorListing from '../DeviceColorListing';
 
 import { renderWithTheme } from '../../test/helpers';
 
+const colors = [
+  { id: 'color-id1', color: 'color-1', colorHex: '#ccc' },
+  { id: 'color-id2', color: 'color-2', colorHex: '#ccc' },
+  { id: 'color-id3', color: 'color-3', colorHex: '#ccc' },
+];
+
 describe('<DeviceColorListing />', () => {
   it('should render a list of colors', () => {
-    const colors = [{ color: 'fff' }, { color: 'fff' }];
-    const { getByTestId } = renderWithTheme(<DeviceColorListing colors={colors} />);
+    const { getByTestId } = renderWithTheme(
+      <DeviceColorListing
+        colors={colors}
+        onClickColor={jest.fn()}
+        selectedColorId='' />
+    );
 
-    expect(getByTestId('device-color-listing').children.length).toBe(2);
+    expect(getByTestId('device-color-listing').children.length).toBe(3);
   });
 
   it('should render a list of colors', () => {
-    const { getByTestId } = renderWithTheme(<DeviceColorListing colors={[]} />);
+    const { getByTestId } = renderWithTheme(
+      <DeviceColorListing
+        colors={[]}
+        onClickColor={jest.fn()}
+        selectedColorId='' />
+    );
 
     expect(getByTestId('device-color-listing').children.length).toBe(0);
   });
