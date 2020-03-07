d73541383a537b75ce035cf4de3e17ae4170adeb
robertschaap <robert.schaap@mac.com>
2020-03-07 12:46:31 +0100

    test(ErrorPageContainer): setup test

---
+++ src/containers/__tests__/ErrorPageContainer.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ErrorPageContainer from '../ErrorPageContainer';
+
+import { renderWithProviders } from '../../test/helpers';
+
+describe('<ErrorPageContainer />', () => {
+  it('should render without crashing', () => {
+    renderWithProviders(<ErrorPageContainer />);
+  });
+});
