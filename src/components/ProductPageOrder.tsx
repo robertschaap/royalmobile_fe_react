6d39dcb4c0b6646c8d9e54b34d4360db2e57a41b
robertschaap <robert.schaap@mac.com>
2020-03-08 10:07:34 +0100

    feat(ProductPageOrder): add click handlers

--- src/components/ProductPageOrder.tsx
+++ src/components/ProductPageOrder.tsx
@@ -11,17 +11,26 @@ const OrderButton = styled(Button)`
   margin-bottom: ${({ theme }) => theme.spacing(2)};
 `;
 
-const ProductPageOrder: React.FC = () => {
+interface ProductPageOrderProps {
+  onClickOrder(): void;
+  onClickRenewContract(): void;
+}
+
+const ProductPageOrder: React.FC<ProductPageOrderProps> = (props) => {
+  const { onClickOrder, onClickRenewContract } = props;
+
   return (
     <PageSection>
       <GridBase>
         <GridItem lg={2}>
           <OrderButton
-            variant="primary">
+            variant="primary"
+            onClick={onClickOrder}>
             {useContentCopy('order.readyToOrder')}
           </OrderButton>
           <Button
-            variant="secondary">
+            variant="secondary"
+            onClick={onClickRenewContract}>
             {useContentCopy('order.renewContract')}
           </Button>
         </GridItem>
