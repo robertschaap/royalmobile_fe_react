ff1c2feb00ca72ebb47eb4bcb887b08e6acef461
robertschaap <robert.schaap@mac.com>
2020-03-08 09:32:55 +0100

    test(useContentCopy): mock console.warn

--- src/hooks/__tests__/useContentCopy.test.tsx
+++ src/hooks/__tests__/useContentCopy.test.tsx
@@ -1,12 +1,26 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useContentCopy } from '../useContentCopy';
 
-it('should render a message when a correct id is passed', () => {
-  const { result } = renderHook(() => useContentCopy('navigation.home'));
-  expect(result.current).toEqual('Home');
-});
+const warn = console.warn;
+const mockedWarn = jest.fn();
 
-it('should render the raw id when an incorrect id is passed', () => {
-  const { result } = renderHook(() => useContentCopy('Not an id'));
-  expect(result.current).toEqual('Not an id');
+describe('useContentCopy', () => {
+  beforeEach(() => {
+    console.warn = mockedWarn;
+  });
+
+  afterEach(() => {
+    console.warn = warn;
+  });
+
+  it('should render a message when a correct id is passed', () => {
+    const { result } = renderHook(() => useContentCopy('navigation.home'));
+    expect(result.current).toEqual('Home');
+  });
+
+  it('should render the raw id when an incorrect id is passed', () => {
+    const { result } = renderHook(() => useContentCopy('Not an id'));
+    expect(result.current).toEqual('Not an id');
+    expect(mockedWarn).toHaveBeenCalled();
+  });
 });
