6cf84b697bfb6c7958ef5063693222ada27271c5
robertschaap <robert.schaap@mac.com>
2020-03-08 09:33:50 +0100

    test(Toggle): add change handler

--- src/components/__test__/Toggle.test.tsx
+++ src/components/__test__/Toggle.test.tsx
@@ -5,13 +5,21 @@ import { renderWithTheme } from '../../test/helpers';
 
 describe('<Toggle />', () => {
   it('should have the checked attribute when it is active', () => {
-    const { getByTestId } = renderWithTheme(<Toggle isActive={true} />);
+    const { getByTestId } = renderWithTheme(
+      <Toggle
+        isActive={true}
+        onChange={jest.fn()} />
+    );
 
     expect(getByTestId('toggle-input')).toHaveAttribute('checked');
   });
 
   it('should not have the checked attribute when it is not active', () => {
-    const { getByTestId } = renderWithTheme(<Toggle isActive={false} />);
+    const { getByTestId } = renderWithTheme(
+      <Toggle
+        isActive={false}
+        onChange={jest.fn()} />
+    );
 
     expect(getByTestId('toggle-input')).not.toHaveAttribute('checked');
   });
