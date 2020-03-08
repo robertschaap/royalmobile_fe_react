3896d7bba17650fe41bfbdf0124900d15242b8db
robertschaap <robert.schaap@mac.com>
2020-03-08 09:46:30 +0100

    feat(stubs): add product stubs

--- src/stubs/index.ts
+++ src/stubs/index.ts
@@ -1,10 +1,11 @@
-import { getProducts, getSubscriptions } from './apiResponses';
+import { getProduct, getProducts, getSubscriptions } from './apiResponses';
 import { Server } from 'miragejs';
 import routes from '../constants/routes';
 
 export default () => new Server({
   environment: 'development',
   routes() {
+    this.get(routes.API_GET_PRODUCT, getProduct);
     this.get(routes.API_GET_PRODUCTS, getProducts);
     this.get(routes.API_GET_SUBSCRIPTIONS, getSubscriptions);
   },
