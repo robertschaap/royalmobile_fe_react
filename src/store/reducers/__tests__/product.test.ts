e8ea09ce39be6e40125ef1e2c32d1e29523288fa
robertschaap <robert.schaap@mac.com>
2020-03-11 21:39:15 +0100

    test(productReducer): add tests

--- src/store/reducers/__tests__/product.test.ts
+++ src/store/reducers/__tests__/product.test.ts
@@ -4,6 +4,10 @@ import {
   fetchProduct,
   fetchProductSuccess,
   fetchProductError,
+  setProductSelectedDurationId,
+  setProductSelectedPaymentPlanId,
+  setProductSelectedSubscriptionId,
+  setProductSelectedVariantId,
   ProductActionTypes,
 } from '../../ducks/product';
 
@@ -15,6 +19,12 @@ describe('Products Reducer', () => {
       isFetching: false,
       hasError: false,
       collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: null,
+      },
     });
   });
 
@@ -23,6 +33,12 @@ describe('Products Reducer', () => {
       isFetching: true,
       hasError: false,
       collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: null,
+      },
     });
   });
 
@@ -31,6 +47,12 @@ describe('Products Reducer', () => {
       isFetching: false,
       hasError: false,
       collection: {},
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: null,
+      },
     });
   });
 
@@ -39,6 +61,68 @@ describe('Products Reducer', () => {
       isFetching: false,
       hasError: true,
       collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: null,
+      },
+    });
+  });
+
+  it('should handle SET_PRODUCT_SELECTED_DURATION_ID', () => {
+    expect(productReducer(undefined, setProductSelectedDurationId('duration_id-1'))).toEqual({
+      isFetching: false,
+      hasError: false,
+      collection: null,
+      selection: {
+        durationId: 'duration_id-1',
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: null,
+      }
+    });
+  });
+
+  it('should handle SET_PRODUCT_SELECTED_PAYMENTPLAN_ID', () => {
+    expect(productReducer(undefined, setProductSelectedPaymentPlanId('payment_plan_id-1'))).toEqual({
+      isFetching: false,
+      hasError: false,
+      collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: 'payment_plan_id-1',
+        subscriptionId: null,
+        variantId: null,
+      }
+    });
+  });
+
+  it('should handle SET_PRODUCT_SELECTED_SUBSCRIPTION_ID', () => {
+    expect(productReducer(undefined, setProductSelectedSubscriptionId('subscription_id-1'))).toEqual({
+      isFetching: false,
+      hasError: false,
+      collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: 'subscription_id-1',
+        variantId: null,
+      }
+    });
+  });
+
+  it('should handle SET_PRODUCT_SELECTED_VARIANT_ID', () => {
+    expect(productReducer(undefined, setProductSelectedVariantId('variant_id-1'))).toEqual({
+      isFetching: false,
+      hasError: false,
+      collection: null,
+      selection: {
+        durationId: null,
+        paymentPlanId: null,
+        subscriptionId: null,
+        variantId: 'variant_id-1',
+      }
     });
   });
 });
