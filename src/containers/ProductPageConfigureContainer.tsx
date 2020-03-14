1d2da8b09c17ba7b516ec97566ee961aa7b4bef1
robertschaap <robert.schaap@mac.com>
2020-03-14 11:07:34 +0100

    feat(ProductPageConfigureContainer): add data from store

--- src/containers/ProductPageConfigureContainer.tsx
+++ src/containers/ProductPageConfigureContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import {
   selectProductSelection,
+  selectSubscriptionsCollection,
   setProductSelectedDurationId,
   setProductSelectedSubscriptionId,
   useDispatch,
@@ -12,19 +13,15 @@ import SubscriptionListing from '../components/SubscriptionListing';
 import PaymentSelector from '../components/PaymentSelector';
 import PageSection from '../components/PageSection';
 import { useContentCopy } from '../hooks';
+import SubscriptionUtil from '../utils/SubscriptionUtil';
 
 const ProductPageConfigureContainer: React.FC = () => {
-  const durations = ['2_year', '1_year'];
-
-  const subscriptions = [
-    { id: '1' },
-    { id: '2' },
-    { id: '3' },
-    { id: '4' },
-  ];
-
   const dispatch = useDispatch();
   const productSelection = useSelector(selectProductSelection);
+  const subscriptions = useSelector(selectSubscriptionsCollection);
+
+  const durations = SubscriptionUtil.getSubscriptionDurations(subscriptions);
+  const subscriptionsByDuration = SubscriptionUtil.getSubscriptionsByDuration(subscriptions, productSelection.durationId || durations[0]);
 
   const onClickDuration = useCallback(id => {
     dispatch(setProductSelectedDurationId(id));
@@ -45,8 +42,8 @@ const ProductPageConfigureContainer: React.FC = () => {
         selectedDurationId={productSelection.durationId || durations[0]} />
       <SubscriptionListing
         onClickSubscription={onClickSubscription}
-        subscriptions={subscriptions}
-        selectedSubscriptionId={productSelection.subscriptionId || subscriptions[0].id} />
+        subscriptions={subscriptionsByDuration}
+        selectedSubscriptionId={productSelection.subscriptionId || subscriptionsByDuration[0].subscriptionId} />
       <PaymentSelector
         onChangeToggle={() => setIsToggleActive(!isToggleActive)}
         isToggleActive={isToggleActive}/>
