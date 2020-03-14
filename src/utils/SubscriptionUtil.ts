0c57f458f6ea34b38679f05ff452decabcbc348d
robertschaap <robert.schaap@mac.com>
2020-03-14 10:15:34 +0100

    feat(SubscriptionUtil): add getSubscriptionsByDuration function

--- src/utils/SubscriptionUtil.ts
+++ src/utils/SubscriptionUtil.ts
@@ -4,11 +4,19 @@ import dedupeArray from '../utils/dedupeArray';
 /**
  * Returns a de-duplicated array of `durationId` keys from an array of `Subscription` objects
  */
-const getSubscriptionDurations = (subscriptions: Subscription[]) => {
+const getSubscriptionDurations = (subscriptions: Subscription[]): string[] => {
   const durationIds = subscriptions.map(subscription => subscription.durationId);
   return dedupeArray(durationIds);
 };
 
+/**
+ * Returns an array of `Subscription` objects filtered by `durationId`
+ */
+const getSubscriptionsByDuration = (subscriptions: Subscription[], durationId: string): Subscription[] => {
+  return subscriptions.filter(subscription => subscription.durationId === durationId);
+};
+
 export default {
   getSubscriptionDurations,
+  getSubscriptionsByDuration,
 };
