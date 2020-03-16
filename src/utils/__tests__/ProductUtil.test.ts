c02d6640c93acf1f0a35cc1af9cf66034cf43593
robertschaap <robert.schaap@mac.com>
2020-03-16 16:55:02 +0100

    test(ProductUtil): setup test for getProductVariantCapacitiesForColor

--- src/utils/__tests__/ProductUtil.test.ts
+++ src/utils/__tests__/ProductUtil.test.ts
@@ -9,6 +9,29 @@ const variants = [{
 }] as ProductVariant[];
 
 describe('ProductUtil', () => {
+  describe('getProductVariantCapacitiesForColor', () => {
+    it('should return an array of capacity properties for a color', () => {
+      expect(
+        ProductUtil.getProductVariantCapacitiesForColor(variants, 'color-id1')
+      ).toEqual([{
+        id: 'variant-id1',
+        capacity: 'capacity-id1',
+      }])
+    });
+
+    it('should return an empty array if no variants are passed', () => {
+      expect(
+        ProductUtil.getProductVariantCapacitiesForColor([], 'color-id1')
+      ).toEqual([])
+    });
+
+    it('should return an empty array if no matches are found', () => {
+      expect(
+        ProductUtil.getProductVariantCapacitiesForColor(variants, 'not-a-valid-id')
+      ).toEqual([])
+    });
+  });
+
   describe('getProductVariantColorsForCapacity', () => {
     it('should return an array of of color properties for a capacity', () => {
       expect(
