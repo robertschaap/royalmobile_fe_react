a09b0122a6284d265cf57f8cecab3da67f0174e0
robertschaap <robert.schaap@mac.com>
2020-03-17 20:04:43 +0100

    style(ContactPage): order imports

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { GridBase, GridItem } from './Grid';
+import ExternalLink from './ExternalLink';
 import IconFacebook from './IconFacebook';
 import IconLinkedIn from './IconLinkedIn';
 import IconTwitter from './IconTwitter';
-import ExternalLink from './ExternalLink';
 import PageSection from './PageSection';
 import SectionHeader from './SectionHeader';
 
