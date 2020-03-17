634ccbadda24c880941383617ddb73e5f38003ae
robertschaap <robert.schaap@mac.com>
2020-03-17 20:23:06 +0100

    feat(ProductPageSummary): setup temporary section

--- src/containers/ProductPageSummaryContainer.tsx
+++ src/containers/ProductPageSummaryContainer.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+
+import {
+  useSelector,
+  selectProductSelection,
+} from '../store';
+
 import { useContentCopy } from '../hooks';
 
 import PageSection from '../components/PageSection';
@@ -6,10 +12,12 @@ import ProductPageSummary from '../components/ProductPageSummary';
 import SectionHeader from '../components/SectionHeader';
 
 const ProductPageSummaryContainer: React.FC = () => {
+  const selection = useSelector(selectProductSelection);
   return (
     <PageSection>
       <SectionHeader>{useContentCopy('product.summingUp')}</SectionHeader>
       <ProductPageSummary />
+      <pre>{JSON.stringify(selection, null, 2)}</pre>
     </PageSection>
   );
 };
