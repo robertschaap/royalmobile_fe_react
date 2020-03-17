dabef8cf1dfa523fd83361cc2252dddafe957b9f
robertschaap <robert.schaap@mac.com>
2020-03-17 20:05:30 +0100

    feat(OrderPage): setup skeleton page

--- src/components/OrderPage.tsx
+++ src/components/OrderPage.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useContentCopy } from '../hooks';
+import noop from '../utils/noop';
+
+import Button from './Button';
+import PageSection from './PageSection';
+import SectionHeader from './SectionHeader';
+
+const OrderButton = styled(Button)`
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
+`;
 
 const OrderPage: React.FC = () => {
   return (
-    <div>OrderPage</div>
+    <PageSection>
+      <SectionHeader>{useContentCopy('Your order')}</SectionHeader>
+      <div>
+        <div>A Cart Item</div>
+        <div>A Cart Item</div>
+      </div>
+      <div>Subtotal</div>
+      <OrderButton
+        variant="primary"
+        onClick={noop}>
+        {useContentCopy('I\'m ready to pay!')}
+      </OrderButton>
+      <Button
+        variant="secondary"
+        onClick={noop}>
+        {useContentCopy('I want to keep shopping')}
+      </Button>
+    </PageSection>
   );
 };
 
