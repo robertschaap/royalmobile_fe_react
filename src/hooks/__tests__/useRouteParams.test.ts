1ff41255268ddd9676776bdc31604d0ec6226036
robertschaap <robert.schaap@mac.com>
2020-03-18 19:41:31 +0100

    test(useRouteParams): add test

---
+++ src/hooks/__tests__/useRouteParams.test.ts
@@ -0,0 +1,36 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useRouteMatch } from 'react-router-dom';
+import { useRouteParams } from '../useRouteParams';
+
+jest.mock('react-router-dom', () => ({
+  useRouteMatch: jest.fn(),
+}));
+
+describe('useRouteParams', () => {
+  afterEach(() => {
+    (useRouteMatch as jest.Mock).mockReset()
+  });
+
+  it('should return an empty object if the route is not matched', () => {
+    const mockReturn = null;
+    (useRouteMatch as jest.Mock).mockReturnValue(mockReturn);
+
+    const { result } = renderHook(() => useRouteParams('/route/:id'));
+
+    expect(useRouteMatch).toHaveBeenCalledWith('/route/:id');
+    expect(useRouteMatch).toHaveReturnedWith(mockReturn);
+    expect(result.current).toEqual({});
+  });
+
+  it('should return an object with params mapped to it if the route is matched', () => {
+    const mockReturn = { params: { id: 'matched-id' } };
+    (useRouteMatch as jest.Mock).mockReturnValue(mockReturn);
+
+    const { result } = renderHook(() => useRouteParams('/route/:id'));
+
+    expect(useRouteMatch).toHaveBeenCalledWith('/route/:id');
+    expect(useRouteMatch).toHaveReturnedWith(mockReturn);
+    expect(result.current).toEqual({ id: 'matched-id' });
+  });
+});
