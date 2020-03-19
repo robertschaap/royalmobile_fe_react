846a47b3e7659659d324d996ad5fe9058240b7fd
robertschaap <robert.schaap@mac.com>
2020-03-19 18:42:47 +0100

    style(SubscriptionUtil): lint fixes

--- src/utils/SubscriptionUtil.ts
+++ src/utils/SubscriptionUtil.ts
@@ -1,11 +1,11 @@
 import { Subscription } from '../types/subscriptions';
-import dedupeArray from '../utils/dedupeArray';
+import dedupeArray from './dedupeArray';
 
 /**
  * Returns a de-duplicated array of `durationId` keys from an array of `Subscription` objects
  */
 const getSubscriptionDurations = (subscriptions: Subscription[]): string[] => {
-  const durationIds = subscriptions.map(subscription => subscription.durationId);
+  const durationIds = subscriptions.map((subscription) => subscription.durationId);
   return dedupeArray(durationIds);
 };
 
@@ -13,7 +13,7 @@ const getSubscriptionDurations = (subscriptions: Subscription[]): string[] => {
  * Returns an array of `Subscription` objects filtered by `durationId`
  */
 const getSubscriptionsByDuration = (subscriptions: Subscription[], durationId: string): Subscription[] => {
-  return subscriptions.filter(subscription => subscription.durationId === durationId);
+  return subscriptions.filter((subscription) => subscription.durationId === durationId);
 };
 
 export default {
