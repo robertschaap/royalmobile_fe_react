cc0d8842a81305383d7a19264d26969d8659f5f1
robertschaap <robert.schaap@mac.com>
2020-03-19 18:41:11 +0100

    style(subscriptions): fix missing semicolon

--- src/types/subscriptions.ts
+++ src/types/subscriptions.ts
@@ -4,7 +4,7 @@ export interface Subscription {
   subscriptionId: string;
   durationId: string;
   data: string;
-  benefits_long: string[],
+  benefits_long: string[];
   benefits_short: string;
   regular_price: string;
 }
