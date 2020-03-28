e2d00d0397056c9236f59616007babfd1ffbce21
robertschaap <robert.schaap@mac.com>
2020-03-28 15:04:30 +0100

    test(Overlay): use different render function

--- src/components/__test__/Overlay.test.tsx
+++ src/components/__test__/Overlay.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Overlay from '../Overlay';
 
-import { renderWithProviders } from '../../test/helpers';
+import { renderWithTheme } from '../../test/helpers';
 
 describe('<Overlay />', () => {
   it('should render without crashing', () => {
-    renderWithProviders(<Overlay />);
+    renderWithTheme(<Overlay />);
   });
 });
