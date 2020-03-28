ba4250c1014abb5e48c1d7db68669871d03ffdb1
robertschaap <robert.schaap@mac.com>
2020-03-28 15:04:44 +0100

    test(Loader): use different render function

--- src/components/__test__/Loader.test.tsx
+++ src/components/__test__/Loader.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Loader from '../Loader';
 
-import { renderWithProviders } from '../../test/helpers';
+import { renderWithTheme } from '../../test/helpers';
 
 describe('<Loader />', () => {
   it('should show the loader when passed a true value', () => {
-    const { queryByTestId } = renderWithProviders(<Loader show={true} />);
+    const { queryByTestId } = renderWithTheme(<Loader show={true} />);
 
     expect(queryByTestId('loader')).not.toBeNull();
   });
 
   it('should not show the loader when passed a false value', () => {
-    const { queryByTestId } = renderWithProviders(<Loader show={false} />);
+    const { queryByTestId } = renderWithTheme(<Loader show={false} />);
 
     expect(queryByTestId('loader')).toBeNull();
   });
