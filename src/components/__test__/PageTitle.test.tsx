0f619d3d7613704732ac376c90607b5c6566a472
robertschaap <robert.schaap@mac.com>
2020-03-29 13:40:18 +0200

    test(PageTitle): rework tests

--- src/components/__test__/PageTitle.test.tsx
+++ src/components/__test__/PageTitle.test.tsx
@@ -5,26 +5,16 @@ import PageTitle from '../PageTitle';
 
 describe('<PageTitle />', () => {
   it('should set the default title if no props are passed', () => {
-    render(<PageTitle />);
+    const { container } = render(<PageTitle />);
 
     expect(document.title).toBe('RoyalMobile');
-  });
-
-  it('should change the site title if passed', () => {
-    render(<PageTitle title="Test"/>);
-
-    expect(document.title).toBe('Test');
+    expect(container.firstChild).toBeNull();
   });
 
   it('it should add the page title if passed', () => {
-    render(<PageTitle page="Test" />);
+    const { container } = render(<PageTitle page="Test" />);
 
     expect(document.title).toBe('RoyalMobile - Test');
-  });
-
-  it('it should change the site and page title if passed', () => {
-    render(<PageTitle title="Test" page="Test" />);
-
-    expect(document.title).toBe('Test - Test');
+    expect(container.firstChild).toBeNull();
   });
 });
