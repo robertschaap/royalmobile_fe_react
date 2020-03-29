814203b8b86e47612ca62ab7b23c88b4c534a411
robertschaap <robert.schaap@mac.com>
2020-03-29 19:58:03 +0200

    feat(ErrorPageContainer): add page title

--- src/containers/ErrorPageContainer.tsx
+++ src/containers/ErrorPageContainer.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import ErrorPage from '../components/ErrorPage';
+import PageTitle from '../components/PageTitle';
 
 const ErrorPageContainer: React.FC = () => {
   return (
     <>
+      <PageTitle page="Error" />
       <ErrorPage />
     </>
   );
