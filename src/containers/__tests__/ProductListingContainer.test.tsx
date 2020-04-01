ab40cc4ee58aff0fe55bb9d5ee26f612f8e8de12
robertschaap <robert.schaap@mac.com>
2020-04-01 20:42:42 +0200

    test(ProductListingContainer): add test scenarios

--- src/containers/__tests__/ProductListingContainer.test.tsx
+++ src/containers/__tests__/ProductListingContainer.test.tsx
@@ -7,4 +7,32 @@ describe('<ProductListingContainer />', () => {
   it('should render without crashing', () => {
     renderWithProviders(<ProductListingContainer />);
   });
+
+  it('should render an empty page if there no products', () => {
+    const { getByTestId } = renderWithProviders(<ProductListingContainer />, {
+      products: {
+        pageNumber: 0,
+        collection: [],
+      },
+    });
+
+    expect(getByTestId('product-listing').children.length).toBe(0);
+    expect(getByTestId('button-secondary')).toBeDisabled();
+  });
+
+  xit('should render a page with products', () => {
+    //
+  });
+
+  xit('should load more products when the load more button is clicked', () => {
+    //
+  });
+
+  xit('should show a loading state when loading more products', () => {
+    //
+  });
+
+  xit('should show an error message when there is an error', () => {
+    //
+  });
 });
