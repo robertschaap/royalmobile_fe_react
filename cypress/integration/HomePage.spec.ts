753bdcce85bf0475c4184bb2e4103782844ccb2f
robertschaap <robert.schaap@mac.com>
2020-04-15 22:45:03 +0200

    test(HomePage): setup basic cypress test

--- cypress/integration/HomePage.spec.ts
+++ cypress/integration/HomePage.spec.ts
@@ -1,10 +1,30 @@
+import routes from '../../src/constants/routes';
+
 context('Homepage', () => {
   before(() => {
     cy.server();
-    cy.visit('/');
+    cy.visit(routes.HOME_PAGE);
   });
 
-  it('should', () => {
+  it('should skip navigation', () => {
+    cy
+      .get('[data-testid="button-secondary"]')
+      .first()
+      .focus()
+      .click();
+  });
 
+  it('should load more products when the load more button is clicked', () => {
+    cy.get('[data-testid="product-listing"]')
+      .siblings('[data-testid="button-secondary"]')
+      .click();
+  });
+
+  it('should go to the product page when a product is clicked', () => {
+    cy.get('[data-testid="product-listing"] a')
+      .last()
+      .click();
+
+    cy.url().should('include', '/product/');
   });
 });
