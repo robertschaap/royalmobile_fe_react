3c5ff06e8325ca7848d36a76863ada416ea9ce8f
robertschaap <robert.schaap@mac.com>
2020-04-28 21:54:10 +0200

    feat(HelpPageContainer): add HelpPage component

--- src/containers/HelpPageContainer.tsx
+++ src/containers/HelpPageContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import HelpPage from '../components/HelpPage';
 import PageTitle from '../components/PageTitle';
 
 const HelpPageContainer: React.FC = () => {
   return (
     <>
       <PageTitle page="Help" />
-      <div>HelpPageContainer</div>
+      <HelpPage />
     </>
   );
 };
