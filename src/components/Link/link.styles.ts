933a4324838d03516a6bbd12bc63307bf4569f39
robertschaap <robert.schaap@mac.com>
2020-05-02 11:51:46 +0200

    refactor(Link): group files into folder

--- src/components/Link/link.styles.ts
+++ src/components/Link/link.styles.ts
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 /**
  * Wraps Link component from react-router-dom and removes link styling.
  */
-const Link = styled(RouterLink)`
+export const Link = styled(RouterLink)`
   color: unset;
   text-decoration: unset;
 `;
-
-export default Link;
