8a2aff1a42e51f9cc9173a103e36762a742192cb
robertschaap <robert.schaap@mac.com>
2020-05-03 11:31:18 +0200

    refactor(GlobalStyle): extracted to styles folder

--- src/containers/ThemeProviderContainer.tsx
+++ src/containers/ThemeProviderContainer.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/global.styles';
 import theme from '../styles/theme';
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    font-family: 'Raleway', sans-serif;
-  }
-
-  a:focus,
-  button:focus {
-    outline: none;
-    box-shadow: 0 0 0 3px ${theme.color.blue};
-  }
-`;
 
 interface ThemeProviderContainerProps {
   children: React.ReactNode;
---
+++ src/styles/global.styles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+
+  a:focus,
+  button:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px ${theme.color.blue};
+  }
+`;
