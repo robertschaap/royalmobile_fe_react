0f5e30ba5ad9985a08f02f473b1c9b6fa7663c70
robertschaap <robert.schaap@mac.com>
2020-05-06 20:15:23 +0200

    test(BusinessPageContainer): setup test

---
+++ src/containers/__tests__/BusinessPageContainer.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConsumerPageContainer from '../ConsumerPageContainer';
+
+import { renderWithProviders } from '../../test/helpers';
+
+describe('<ConsumerPageContainer />', () => {
+  it('should render without crashing', () => {
+    renderWithProviders(<ConsumerPageContainer />);
+  });
+});
