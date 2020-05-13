4f4cfe8da226014bcf66a4ddc52844e3f4e84639
robertschaap <robert.schaap@mac.com>
2020-05-13 20:01:18 +0200

    feat(StoreProviderContainer): do not pass initialState to store

--- src/store/StoreProviderContainer.tsx
+++ src/store/StoreProviderContainer.tsx
@@ -3,14 +3,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 
-import { rootReducer, initialState } from './reducers';
+import { rootReducer } from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
   rootReducer(),
-  initialState,
+  undefined,
   compose(
     applyMiddleware(sagaMiddleware),
     // eslint-disable-next-line no-underscore-dangle
