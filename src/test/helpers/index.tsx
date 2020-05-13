507f19069e0f0edb86419d2368e5db6154c35d16
robertschaap <robert.schaap@mac.com>
2020-05-13 20:16:22 +0200

    test(helpers): conditionally add actual redux store

--- src/test/helpers/index.tsx
+++ src/test/helpers/index.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 import theme from '../../styles/theme';
-import { initialState } from '../../store/reducers';
+import { store as reduxStore } from '../../store';
 
 export const render = renderTest;
 
@@ -21,7 +21,9 @@ export const renderWithTheme = (Component: React.ReactElement) => {
 const mockStore = configureStore([]);
 
 export const renderWithProviders = (Component: React.ReactElement, customInitialState?: Object) => {
-  const store = mockStore(customInitialState || initialState);
+  // Mock the store only when we pass a customInitialState so we can test reducers without loading
+  // the whole store. Otherwise load the proper store with reducers and sagas for integration
+  const store = customInitialState ? mockStore(customInitialState) : reduxStore;
 
   return renderTest(
     <Provider store={store}>
