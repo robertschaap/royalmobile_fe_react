5663085c30e36fd504376087f4fb5a0a3ed264ed
robertschaap <robert.schaap@mac.com>
2020-05-30 13:40:57 +0200

    feat(StringUtil): add price formatting function

--- src/utils/StringUtil.ts
+++ src/utils/StringUtil.ts
@@ -2,6 +2,13 @@ const capitalise = (string: string): string => {
   return string[0].toUpperCase() + string.slice(1);
 };
 
+const formatPrice = (string: string): string => {
+  const [number, decimal = ''] = string.split(',');
+
+  return `${number || '0'},${decimal.padEnd(2, '0')}`;
+};
+
 export default {
   capitalise,
+  formatPrice,
 };
