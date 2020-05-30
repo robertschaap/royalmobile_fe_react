b9f48edca56d05e5b28ed5f87f51019b012bb523
robertschaap <robert.schaap@mac.com>
2020-05-30 13:41:16 +0200

    test(StringUtil): add formatPrice tests

--- src/utils/__tests__/StringUtil.test.ts
+++ src/utils/__tests__/StringUtil.test.ts
@@ -6,3 +6,18 @@ describe('StringUtil', () => {
       expect(StringUtil.capitalise('test test')).toEqual('Test test');
     });
   });
+
+  describe('formatPrice', () => {
+    it('should format a price', () => {
+      expect(StringUtil.formatPrice('1')).toEqual('1,00');
+      expect(StringUtil.formatPrice('1,5')).toEqual('1,50');
+      expect(StringUtil.formatPrice('1,50')).toEqual('1,50');
+    });
+
+    it('should format a price when passed an incomplete price', () => {
+      expect(StringUtil.formatPrice('')).toEqual('0,00');
+      expect(StringUtil.formatPrice(',0')).toEqual('0,00');
+      expect(StringUtil.formatPrice(',00')).toEqual('0,00');
+    });
+  });
+});
