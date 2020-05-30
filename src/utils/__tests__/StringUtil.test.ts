4dd13056511c8e2aa745e76478aac056bb99968c
robertschaap <robert.schaap@mac.com>
2020-05-30 13:40:30 +0200

    test(StringUtil): fix capitalise test

--- src/utils/__tests__/StringUtil.test.ts
+++ src/utils/__tests__/StringUtil.test.ts
@@ -2,6 +2,7 @@ import StringUtil from '../StringUtil';
 
 describe('StringUtil', () => {
   describe('capitalise', () => {
-    expect(StringUtil.capitalise('test test')).toEqual('Test test');
+    it('should capitalise the first letter of a string', () => {
+      expect(StringUtil.capitalise('test test')).toEqual('Test test');
+    });
   });
-});
