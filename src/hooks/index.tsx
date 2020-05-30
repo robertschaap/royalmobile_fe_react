924de792cbb24aca10f7d61e1349a5737455036c
robertschaap <robert.schaap@mac.com>
2020-05-30 15:08:09 +0200

    feat(orderSummary): setup hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { useContentCopy } from './useContentCopy';
 export { useRouteParams } from './useRouteParams';
+export * from './useOrderSummary';
---
+++ src/hooks/useOrderSummary.ts
@@ -0,0 +1,46 @@
+import ProductUtil from '../utils/ProductUtil';
+import SubscriptionUtil from '../utils/SubscriptionUtil';
+import StringUtil from '../utils/StringUtil';
+import { ProductVariant } from '../types/products';
+import { Subscription } from '../types/subscriptions';
+
+export interface OrderSummary {
+  connectionFee: string;
+  monthlyDeviceCost: string;
+  monthlyDiscountTotal: string;
+  monthlyPaymentTotal: string;
+  oneTimeDeviceCost: string;
+  oneTimeDiscountTotal: string;
+  oneTimePaymentTotal: string;
+  selectedSubscription: Subscription;
+  selectedVariant: ProductVariant;
+  shippingFee: string;
+  monthlySubscriptionCost: string;
+}
+
+export const useOrderSummary = (productVariants: ProductVariant[], subscriptions: Subscription[], selection: { [k: string]: string | null }): OrderSummary | null => {
+  const selectedVariant = ProductUtil.getProductVariant(productVariants, selection.variantId || '');
+  const selectedSubscription = SubscriptionUtil.getSubscription(subscriptions, selection.subscriptionId || '');
+
+  if (!selectedVariant || !selectedSubscription) {
+    return null;
+  }
+
+  const calculatedOneTimeDiscount = (Number(selectedVariant.regular_price) - Number(selectedVariant.discounted_price)).toString();
+
+  return {
+    // TODO: ideally not expose these as if muddles the interface of the hook
+    selectedSubscription,
+    selectedVariant,
+
+    monthlySubscriptionCost: StringUtil.formatPrice(selectedSubscription.regular_price),
+    connectionFee: '0,00',
+    monthlyDeviceCost: '0,00',
+    monthlyDiscountTotal: '0,00',
+    monthlyPaymentTotal: StringUtil.formatPrice(selectedSubscription.regular_price),
+    oneTimeDeviceCost: StringUtil.formatPrice(selectedVariant.regular_price),
+    oneTimeDiscountTotal: StringUtil.formatPrice(calculatedOneTimeDiscount),
+    oneTimePaymentTotal: StringUtil.formatPrice(selectedVariant.discounted_price),
+    shippingFee: '0,00',
+  };
+};
