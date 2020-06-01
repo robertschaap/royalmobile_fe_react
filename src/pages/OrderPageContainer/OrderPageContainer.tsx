cca3ed0fda40740d02b59973f5f3f9ac1dfc5f53
robertschaap <robert.schaap@mac.com>
2020-06-01 21:35:39 +0200

    feat(OrderPageContainer): add debug data

--- src/pages/OrderPageContainer/OrderPageContainer.tsx
+++ src/pages/OrderPageContainer/OrderPageContainer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { selectProductSelection, useSelector } from '../../store';
 
 import OrderPage from './OrderPage';
 import PageTitle from '../../components/PageTitle';
 
 const OrderPageContainer: React.FC = () => {
+  const selection = useSelector(selectProductSelection);
+
   return (
     <>
       <PageTitle page="Order" />
       <OrderPage />
+      <pre>{JSON.stringify(selection, null, 2)}</pre>
     </>
   );
 };
