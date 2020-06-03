78b0e0536326b5bafda1376393cecd24adecf227
robertschaap <robert.schaap@mac.com>
2020-06-03 22:17:58 +0200

    test(cart): add tests for fetch cart success and error actions

--- src/store/reducers/__tests__/cart.test.ts
+++ src/store/reducers/__tests__/cart.test.ts
@@ -2,8 +2,12 @@ import { cartReducer, initialCartState } from '../cart';
 
 import {
   fetchCart,
+  fetchCartSuccess,
   CartActionTypes,
+  fetchCartError,
 } from '../../ducks/cart';
+
+import { Cart } from '../../../types/cart';
 
 describe('Cart Reducer', () => {
   it('should return the initial state', () => {
@@ -21,6 +25,33 @@ describe('Cart Reducer', () => {
     expect(cartReducer(initialCartState, fetchCart(''))).toEqual({
       isFetching: true,
       hasError: false,
+      collection: {
+        id: null,
+        items: [],
+      },
+    });
+  });
+
+  it('should handle FETCH_CART_SUCCESS', () => {
+    const cart = {
+      id: 'cart-id',
+      items: [],
+    } as Cart;
+
+    expect(cartReducer(initialCartState, fetchCartSuccess(cart))).toEqual({
+      isFetching: false,
+      hasError: false,
+      collection: {
+        id: 'cart-id',
+        items: [],
+      },
+    });
+  });
+
+  it('should handle FETCH_CART_ERROR', () => {
+    expect(cartReducer(initialCartState, fetchCartError('error'))).toEqual({
+      isFetching: false,
+      hasError: true,
       collection: {
         id: null,
         items: [],
