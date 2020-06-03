113af1b9f6babed7f8803763805bd7fbdbadefa4
robertschaap <robert.schaap@mac.com>
2020-06-03 21:17:03 +0200

    test(cart): fix faulty cart reducer test

--- src/store/reducers/__tests__/cart.test.ts
+++ src/store/reducers/__tests__/cart.test.ts
@@ -1,8 +1,9 @@
-import { initialCartState } from '../cart';
+import { cartReducer, initialCartState } from '../cart';
+import { CartActionTypes } from '../../ducks/cart';
 
 describe('Cart Reducer', () => {
   it('should return the initial state', () => {
-    expect(initialCartState).toEqual({
+    expect(cartReducer(initialCartState, {} as CartActionTypes)).toEqual({
       isFetching: false,
       hasError: false,
       collection: {
