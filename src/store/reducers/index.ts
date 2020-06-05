45e0d24c44430618c72438e8c6ce1625a1627fd9
robertschaap <robert.schaap@mac.com>
2020-06-05 21:45:50 +0200

    feat(store): add cart to rootreducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
+import { cartReducer } from './cart';
 import { productReducer } from './product';
 import { productsReducer } from './products';
 import { subscriptionsReducer } from './subscriptions';
 
 export const rootReducer = () => combineReducers({
+  cart: cartReducer,
   product: productReducer,
   products: productsReducer,
   subscriptions: subscriptionsReducer,
