dce435616ea78cedbd8f544cc9c62c3ed867b7a2
robertschaap <robert.schaap@mac.com>
2020-06-11 17:51:33 +0200

    feat(FormFields): add select

--- src/components/FormFields/FormFields.tsx
+++ src/components/FormFields/FormFields.tsx
@@ -48,3 +48,57 @@ export const Input: React.FC<InputProps> = (props) => {
     </FormGroup>
   );
 };
+
+interface SelectProps {
+  error: string;
+  hasError: boolean;
+  hint?: string;
+  isRequired?: boolean;
+  isValid: boolean;
+  label: string;
+  name: string;
+  onChange(v: any): void;
+  value: string;
+  options: {
+    value: string;
+    label: string;
+  }[];
+}
+
+export const Select: React.FC<SelectProps> = (props) => {
+  const {
+    error,
+    hasError,
+    hint,
+    isRequired,
+    isValid,
+    label,
+    name,
+    onChange,
+    value,
+    options,
+  } = props;
+
+  return (
+    <FormGroup
+      error={error}
+      hasError={hasError}
+      hint={hint}
+      isRequired={isRequired}
+      isValid={isValid}
+      label={label}>
+      <S.SelectField
+        name={name}
+        onChange={onChange}
+        value={value}>
+        {options.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </S.SelectField>
+    </FormGroup>
+  );
+};
--- src/components/FormFields/form-fields.styles.ts
+++ src/components/FormFields/form-fields.styles.ts
@@ -11,3 +11,20 @@ export const InputField = styled.input`
     background-color: ${({ theme }) => theme.color.lightGrey};
   }
 `;
+
+export const SelectField = styled.select`
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  border: unset;
+  border-radius: unset;
+  outline: none;
+  background-color: unset;
+
+  padding: ${({ theme }) => theme.spacing(2)};
+  border-bottom: 2px solid ${({ theme }) => theme.color.grey};
+
+  &:focus {
+    border-bottom: 2px solid ${({ theme }) => theme.color.blue};
+    background-color: ${({ theme }) => theme.color.lightGrey};
+  }
+`;
