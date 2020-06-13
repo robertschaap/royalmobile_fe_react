ded650553665ae45bb9e2aedae586a4eb157d19c
robertschaap <robert.schaap@mac.com>
2020-06-13 22:44:44 +0200

    feat(HelpPage): add content

--- src/pages/HelpPage/HelpPage/HelpPage.tsx
+++ src/pages/HelpPage/HelpPage/HelpPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as S from './help-page.styles';
 import routes from '../../../constants/routes';
+import { useContentCopy } from '../../../hooks';
 
 import { GridBase } from '../../../components/Grid';
 import Link from '../../../components/Link';
@@ -10,7 +11,7 @@ import SectionHeader from '../../../components/SectionHeader';
 const HelpPage: React.FC = () => {
   return (
     <PageSection>
-      <SectionHeader>Help</SectionHeader>
+      <SectionHeader>{useContentCopy('pages.help')}</SectionHeader>
       <GridBase>
         <S.Section md={2} lg={2}>
           <p>If you need help with anything, please feel free to visit our <Link to={routes.CONTACT_PAGE}>contact page</Link>.</p>
