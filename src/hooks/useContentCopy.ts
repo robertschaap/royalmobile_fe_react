ad476989dcbe690324d65180e1ae4e85017c6f5e
robertschaap <robert.schaap@mac.com>
2020-06-15 21:25:19 +0200

    feat(useContentCopy): remove context from hook

--- src/hooks/useContentCopy.ts
+++ src/hooks/useContentCopy.ts
@@ -1,13 +1,8 @@
-import { createContext, useContext } from 'react';
-import contentCopy, { ContentCopy } from '../constants/contentCopy';
-
-const ContentContext = createContext<ContentCopy>(contentCopy);
+import contentCopy from '../constants/contentCopy';
 
 export const useContentCopy = (messageId: string): string => {
-  const copy = useContext(ContentContext);
-
-  if (copy && copy[messageId]) {
-    return copy[messageId];
+  if (contentCopy && contentCopy[messageId]) {
+    return contentCopy[messageId];
   }
 
   // eslint-disable-next-line no-console
