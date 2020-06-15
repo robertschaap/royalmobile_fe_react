e6fe49cc6bfcb0839f4dc71192265071ee037f09
robertschaap <robert.schaap@mac.com>
2020-06-15 17:20:51 +0200

    test(cypress): fix linting error in cypress namespace

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     getTestId(dataTestId: string): Chainable<any>;
   }
 }
