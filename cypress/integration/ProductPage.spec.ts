95675edc0e32f10bdd1641e15f4a392eadf7b500
robertschaap <robert.schaap@mac.com>
2020-06-16 19:26:55 +0200

    test(ProductPage): setup cypress test

--- cypress/integration/ProductPage.spec.ts
+++ cypress/integration/ProductPage.spec.ts
@@ -1,8 +1,46 @@
-import routes from '../../src/constants/routes';
-
-context('Homepage', () => {
+context('Product Page', () => {
   before(() => {
     cy.server();
-    cy.visit(routes.PRODUCT_PAGE);
+    cy.visit('/product/apple-iphonex');
+  });
+
+  it('should select a color when one is clicked', () => {
+    cy.getTestId('device-color-listing')
+      .children()
+      .random()
+      .click();
+  });
+
+  it('should select a device capacity when one is clicked', () => {
+    cy.getTestId('device-capacity-listing')
+      .children()
+      .random()
+      .click();
+  });
+
+  it('should select a duration when one is clicked', () => {
+    cy.getTestId('duration-listing')
+      .children()
+      .random()
+      .click();
+  });
+
+  it('should select a subscription when one is clicked', () => {
+    cy.getTestId('subscription-listing')
+      .children()
+      .random()
+      .click();
+  });
+
+  it('should toggle payment plan options when the toggle button is clicked', () => {
+    cy.getTestId('toggle')
+      .click();
+  });
+
+  it('should go to the order page when the submit button is clicked', () => {
+    cy.getTestId('button-primary')
+      .click();
+
+    cy.url().should('include', '/order');
   });
 });
