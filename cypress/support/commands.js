073a2ac222a51e892baf8855d602a516aeb69e84
robertschaap <robert.schaap@mac.com>
2020-06-16 19:21:13 +0200

    test(cypress): setup random item command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,10 @@
 Cypress.Commands.add('getTestId', (dataTestId) => {
   cy.get(`[data-testid=${dataTestId}]`);
 });
+
+Cypress.Commands.add('random', { prevSubject: true }, (subject) => {
+  if (subject.length) {
+    const random = Math.floor(Math.random() * subject.length);
+    return subject[random];
+  }
+});
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare namespace Cypress {
   interface Chainable<Subject> {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     getTestId(dataTestId: string): Chainable<any>;
+    random(): Chainable<any>;
   }
 }
