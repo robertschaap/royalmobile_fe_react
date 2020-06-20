67b494577f6866bab6a925f9a1ac9dfcf265771d
robertschaap <robert.schaap@mac.com>
2020-06-20 15:25:58 +0200

    test(Footer): add test for redirect

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import Footer from '.';
 
 import { renderWithProviders } from '../../test/helpers';
 
 describe('<Footer />', () => {
-  it('should render without crashing', () => {
-    renderWithProviders(<Footer />);
+  it('should redirect the user to the contact page', () => {
+    const { getByTestId } = renderWithProviders(<Footer />);
+
+    fireEvent.click(getByTestId('footer-contact-link'));
+
+    expect(window.location.pathname).toContain('/contact');
   });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,9 @@ import Link from '../Link';
 const Footer: React.FC = () => {
   return (
     <S.FooterBase>
-      <Link to={routes.CONTACT_PAGE}>
+      <Link
+        data-testid="footer-contact-link"
+        to={routes.CONTACT_PAGE}>
         {ContentUtil('pages.contact')}
       </Link>
       <div><b>{ContentUtil('common.royal')}</b>{ContentUtil('common.mobile')} 2020</div>
