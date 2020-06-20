df640e461458729b9dda1adf4b6bd73ec1684407
robertschaap <robert.schaap@mac.com>
2020-06-20 15:34:52 +0200

    test(FormFields): add tests for fields

--- src/components/FormFields/FormFields.test.tsx
+++ src/components/FormFields/FormFields.test.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Input } from './FormFields';
+import { Input, Select } from './FormFields';
 
 import { renderWithProviders } from '../../test/helpers';
 
 describe('<FormFields />', () => {
-  it('should render without crashing', () => {
-    renderWithProviders(<Input />);
+  describe('Input', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(
+        <Input
+          isValid
+          label="input"
+          name="name"
+          onBlur={jest.fn}
+          onChange={jest.fn}
+          value=""
+        />,
+      );
+    });
+  });
+
+  describe('Select', () => {
+    it('should render without crashing', () => {
+      renderWithProviders(
+        <Select
+          isValid
+          label="input"
+          name="name"
+          onBlur={jest.fn}
+          onChange={jest.fn}
+          options={[]}
+          value=""
+        />,
+      );
+    });
   });
 });
