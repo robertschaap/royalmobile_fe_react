587062d7817639f4a7a9ea2eb4042e02bb71a42a
robertschaap <robert.schaap@mac.com>
2020-06-21 20:40:18 +0200

    test(OrderSummaryText): add missing props

--- src/pages/ProductPage/OrderSummaryText/OrderSummaryText.test.tsx
+++ src/pages/ProductPage/OrderSummaryText/OrderSummaryText.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import OrderSummaryText from '.';
 
 import { renderWithTheme } from '../../../test/helpers';
+import { OrderSummary } from '../../../hooks';
 
 describe('<OrderSummaryText />', () => {
   it('should render without crashing', () => {
-    renderWithTheme(<OrderSummaryText />);
+    renderWithTheme(
+      <OrderSummaryText
+        deviceName="device-name"
+        orderSummary={{} as OrderSummary} />,
+    );
   });
 });
