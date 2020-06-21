19f4ea68bddd87da858138b23dfa1a2bb4507114
robertschaap <robert.schaap@mac.com>
2020-06-21 20:41:12 +0200

    test(PaymentSelector): remove redundant props

--- src/pages/ProductPage/PaymentSelector/PaymentSelector.test.tsx
+++ src/pages/ProductPage/PaymentSelector/PaymentSelector.test.tsx
@@ -5,10 +5,6 @@ import { renderWithTheme } from '../../../test/helpers';
 
 describe('<PaymentSelector />', () => {
   it('should render without crashing', () => {
-    renderWithTheme(
-      <PaymentSelector
-        isToggleActive={true}
-        onChangeToggle={jest.fn()} />,
-    );
+    renderWithTheme(<PaymentSelector />);
   });
 });
