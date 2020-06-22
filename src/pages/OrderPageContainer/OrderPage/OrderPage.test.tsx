675f1e4ed01cb08332769f1c8ac14252a27b101b
robertschaap <robert.schaap@mac.com>
2020-06-22 14:56:26 +0200

    tests(Orderpage): setup test

--- src/pages/OrderPageContainer/OrderPage/OrderPage.test.tsx
+++ src/pages/OrderPageContainer/OrderPage/OrderPage.test.tsx
@@ -2,9 +2,45 @@ import React from 'react';
 import OrderPage from '.';
 
 import { renderWithTheme } from '../../../test/helpers';
+import { Server, stubsServer } from '../../../stubs';
+import { Cart } from '../../../types/cart';
+
+let cart: Cart;
+let server: Server;
 
 describe('<OrderPage />', () => {
+  beforeAll(() => {
+    server = stubsServer('test');
+    server.create('product');
+    server.create('subscription');
+
+    cart = {
+      id: '',
+      items: [{
+        product: server.db.product[0],
+        subscription: server.db.susbcription[0],
+        totals: {
+          monthly_price: '0,00',
+          onetime_price: '0,00',
+        },
+      }],
+      totals: {
+        monthly_price: '',
+        onetime_price: '',
+      },
+    };
+
+    cart.items.push();
+  });
+
   it('should render without crashing', () => {
-    renderWithTheme(<OrderPage />);
+    renderWithTheme(
+      <OrderPage
+        cart={cart}
+        onClickOrder={jest.fn}
+        onClickRemove={jest.fn}
+        onClickReturn={jest.fn}
+      />,
+    );
   });
 });
