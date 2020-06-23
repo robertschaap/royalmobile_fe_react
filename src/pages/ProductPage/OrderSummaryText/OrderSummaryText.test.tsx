a214252be5ed44e4b24572fa9f55b022ad3da9fd
robertschaap <robert.schaap@mac.com>
2020-06-23 17:58:50 +0200

    test(OrderSummaryText): fix failing test

--- src/pages/ProductPage/OrderSummaryText/OrderSummaryText.test.tsx
+++ src/pages/ProductPage/OrderSummaryText/OrderSummaryText.test.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import { ProductSelection, ProductVariant } from '../../../types/products';
+import { Subscription } from '../../../types/subscriptions';
+import { useOrderSummary, OrderSummary } from '../../../hooks';
 import OrderSummaryText from '.';
 
 import { renderWithTheme } from '../../../test/helpers';
-import { OrderSummary } from '../../../hooks';
+import { Server, stubsServer } from '../../../stubs';
+
+let server: Server;
+
+const variant = {
+  variantId: 'variant-id',
+  regular_price: '2',
+  discounted_price: '1',
+} as ProductVariant;
+
+const subscription = {
+  subscriptionId: 'subscription-id',
+  regular_price: '2',
+} as Subscription;
 
 describe('<OrderSummaryText />', () => {
+  beforeAll(() => {
+    server = stubsServer('test');
+  });
+
+  afterAll(() => {
+    server.shutdown();
+  });
+
   it('should render without crashing', () => {
+    const selection = { variantId: 'variant-id', subscriptionId: 'subscription-id' } as ProductSelection;
+    // @ts-expect-error
+    const summary: OrderSummary = useOrderSummary([variant], [subscription], selection);
+
     renderWithTheme(
       <OrderSummaryText
         deviceName="device-name"
-        orderSummary={{} as OrderSummary} />,
+        orderSummary={summary} />,
     );
   });
 });
