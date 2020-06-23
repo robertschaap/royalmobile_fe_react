0a46473caed92d070735c3646a0a3ff97c9e19ab
robertschaap <robert.schaap@mac.com>
2020-06-23 10:53:50 +0200

    test(ProductPageCustomiseContainer): add tests

--- src/pages/ProductPage/ProductPageCustomiseContainer.test.tsx
+++ src/pages/ProductPage/ProductPageCustomiseContainer.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import { Product } from '../../types/products';
+import * as productDuck from '../../store/ducks/product';
 import ProductPageCustomiseContainer from './ProductPageCustomiseContainer';
 
 import { renderWithProviders } from '../../test/helpers';
 import { Server, stubsServer } from '../../stubs';
-import { Product } from '../../types/products';
-
-// TODO: add selection tests
 
 let server: Server;
 
@@ -36,8 +36,55 @@ describe('<ProductPageCustomiseContainer />', () => {
     expect(getByTestId('product-page-customise')).toBeInTheDocument();
   });
 
+  it('should change the selected color', async () => {
+    const setProductSelectedVariantIdSpy = jest.spyOn(productDuck, 'setProductSelectedVariantId');
+    const selectProductSelectionSpy = jest.spyOn(productDuck, 'selectProductSelection');
+    const product: Product = server.db.products[0];
+
+    const { getByTestId } = renderWithProviders(
+      <ProductPageCustomiseContainer
+        product={product} />,
+      {
+        product: {
+          selection: {
+            variantId: product.variants[0].variantId,
+          },
+        },
+      },
+    );
+
+    fireEvent.click(getByTestId('device-color-listing').children[1]);
+
+    expect(setProductSelectedVariantIdSpy).toHaveBeenLastCalledWith(product.variants[1].variantId);
+    expect(selectProductSelectionSpy).toReturnWith({ variantId: product.variants[1].variantId });
+  });
+
+  it('should change the selected capacity', async () => {
+    const setProductSelectedVariantIdSpy = jest.spyOn(productDuck, 'setProductSelectedVariantId');
+    const selectProductSelectionSpy = jest.spyOn(productDuck, 'selectProductSelection');
+    const product: Product = server.db.products[0];
+
+    const { getByTestId } = renderWithProviders(
+      <ProductPageCustomiseContainer
+        product={product} />,
+      {
+        product: {
+          selection: {
+            variantId: product.variants[0].variantId,
+          },
+        },
+      },
+    );
+
+    fireEvent.click(getByTestId('device-capacity-listing').children[1]);
+
+    expect(setProductSelectedVariantIdSpy).toHaveBeenLastCalledWith(product.variants[2].variantId);
+    expect(selectProductSelectionSpy).toReturnWith({ variantId: product.variants[2].variantId });
+  });
+
   it('should not show the customise section if there is no selection', () => {
     const product: Product = server.db.products[0];
+
     const { queryByTestId } = renderWithProviders(
       <ProductPageCustomiseContainer
         product={product} />,
