ee5029b6ea8ba47d49d887eaa97df06c96a336c1
robertschaap <robert.schaap@mac.com>
2020-06-24 15:38:48 +0200

    test(ProductPageSummary): add test

--- src/pages/ProductPage/ProductPageSummary/ProductPageSummary.test.tsx
+++ src/pages/ProductPage/ProductPageSummary/ProductPageSummary.test.tsx
@@ -1,10 +1,40 @@
+/* eslint-disable prefer-destructuring */
 import React from 'react';
+import { ProductSelection, Product } from '../../../types/products';
+import { Subscription } from '../../../types/subscriptions';
+import { useOrderSummary, OrderSummary } from '../../../hooks';
 import ProductPageSummary from '.';
 
 import { renderWithTheme } from '../../../test/helpers';
+import { Server, stubsServer } from '../../../stubs';
+
+let server: Server;
 
 describe('<ProductPageSummary />', () => {
+  beforeAll(() => {
+    server = stubsServer('test');
+    server.create('product');
+    server.create('subscription');
+  });
+
+  afterAll(() => {
+    server.shutdown();
+  });
+
   it('should render without crashing', () => {
-    renderWithTheme(<ProductPageSummary />);
+    const product: Product = server.db.products[0];
+    const subscriptions: Subscription[] = server.db.subscriptions;
+    const selection = {
+      variantId: product.variants[0].variantId,
+      subscriptionId: subscriptions[0].subscriptionId,
+    };
+    // @ts-expect-error
+    const summary: OrderSummary = useOrderSummary(product.variants, Array.from(subscriptions), selection);
+
+    renderWithTheme(
+      <ProductPageSummary
+        deviceModel="device-model"
+        orderSummary={summary} />,
+    );
   });
 });
