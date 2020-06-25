52be2d4684f62369e4eb5d62a042a9f0ed60a14a
robertschaap <robert.schaap@mac.com>
2020-06-25 11:03:08 +0200

    test(OrderDetailsPage): setup cypress test

--- cypress/integration/OrderDetailsPage.spec.ts
+++ cypress/integration/OrderDetailsPage.spec.ts
@@ -1,8 +1,31 @@
-import routes from '../../src/constants/routes';
+context('Order Details Page', () => {
+  beforeEach(() => {
+    cy.server()
+      .visit('/product/apple-iphonex')
+      .getTestId('place-order-button')
+      .click()
+      .getTestId('button-primary')
+      .click();
+  });
 
-context('Homepage', () => {
-  before(() => {
-    cy.server();
-    cy.visit(routes.ORDER_DETAILS_PAGE);
+  it('should remove a cartItem when the remove button is clicked', () => {
+    cy.getTestId('initials')
+      .type('initials')
+      .getTestId('lastname')
+      .type('lastname')
+      .getTestId('email')
+      .type('email')
+      .getTestId('zipcode')
+      .type('zipcode')
+      .getTestId('housenumber')
+      .type('housenumber')
+      .getTestId('iban')
+      .type('iban');
+
+    cy.getTestId('finalise-order-button')
+      .click();
+
+    cy.url()
+      .should('include', '/thankyou');
   });
 });
