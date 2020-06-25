83dcc06f354c88df37adf53f30b5e1235fd58aba
robertschaap <robert.schaap@mac.com>
2020-06-25 10:13:32 +0200

    test(OrderDetailsPage): add test for focus

--- src/pages/OrderDetailsPage/OrderDetailsPage/OrderDetailsPage.test.tsx
+++ src/pages/OrderDetailsPage/OrderDetailsPage/OrderDetailsPage.test.tsx
@@ -5,6 +5,16 @@ import OrderDetailsPage from '.';
 import { renderWithTheme } from '../../../test/helpers';
 
 describe('<OrderDetailsPage />', () => {
+  it('should focus the first field', () => {
+    const { getByTestId } = renderWithTheme(
+      <OrderDetailsPage
+        onClickReturn={jest.fn}
+        onSubmitOrder={jest.fn} />,
+    );
+
+    expect(getByTestId('initials')).toHaveFocus();
+  });
+
   it('should submit order details', () => {
     const onSubmitOrder = jest.fn();
 
