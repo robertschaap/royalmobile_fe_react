a52d77ac8dcdcacac8ece63b661148b2febaf690
robertschaap <robert.schaap@mac.com>
2020-07-04 15:11:02 +0200

    feat(DeviceImage): setup component

---
+++ src/components/DeviceImage/DeviceImage.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+interface DeviceImageProps {
+  modelId: string;
+  color: string;
+}
+
+const DeviceImage: React.FC<DeviceImageProps> = (props) => {
+  const { color, modelId } = props;
+
+  const [hasError, setHasError] = useState(false);
+
+  if (hasError) {
+    return (
+      <img
+        alt="phone"
+        src="/images/fallback.png"
+        width="100%" />
+    );
+  }
+
+  return (
+    <img
+      alt="phone"
+      onError={() => setHasError(true)}
+      src={`/images/${modelId}-${color}.png`}
+      width="100%" />
+  );
+};
+
+export default DeviceImage;
