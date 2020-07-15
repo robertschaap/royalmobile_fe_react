6f7f333a37eba48f6fd0b891987a12a1d7e5a6bf
robertschaap <robert.schaap@mac.com>
2020-07-15 22:53:59 +0200

    refactor(useRouteParams): change ternary to nullish coalesce

--- src/hooks/useRouteParams.ts
+++ src/hooks/useRouteParams.ts
@@ -2,5 +2,5 @@ import { useRouteMatch } from 'react-router-dom';
 
 export const useRouteParams = (route: string): Record<string, string> => {
   const match = useRouteMatch(route);
-  return match === null ? {} : match.params;
+  return match?.params ?? {};
 };
