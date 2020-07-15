16b7b892f029ff9dc7af941882e41001694e9fa9
robertschaap <robert.schaap@mac.com>
2020-07-15 22:54:18 +0200

    fix(IconFacebook): remove redundant id

--- src/components/Icons/IconFacebook.tsx
+++ src/components/Icons/IconFacebook.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 const IconFacebook: React.FC = () => {
   return (
     <svg
-      id="facebook"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 128 128">
       <rect
