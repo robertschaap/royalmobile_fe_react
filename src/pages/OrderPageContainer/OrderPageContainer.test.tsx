8118cdcd7749b06aeaaeb777f40c0ff551575b0c
robertschaap <robert.schaap@mac.com>
2020-09-19 09:32:41 +0200

    test(OrderPageContainer): test cart states

--- src/pages/OrderPageContainer/OrderPageContainer.test.tsx
+++ src/pages/OrderPageContainer/OrderPageContainer.test.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import * as cartDuck from '../../store/ducks/cart';
 import { Server, stubsServer } from '../../stubs';
 import { renderWithProviders } from '../../test/helpers';
+import { Cart } from '../../types/cart';
 import StorageUtil from '../../utils/StorageUtil';
 
 import OrderPageContainer from './OrderPageContainer';
 
 let server: Server;
+let cart: Cart;
 
 jest.mock('../../utils/StorageUtil', () => ({
   clearProductSelection: jest.fn(),
@@ -17,105 +19,127 @@ jest.mock('../../utils/StorageUtil', () => ({
 }));
 
 describe('<OrderPageContainer />', () => {
-  beforeAll(() => {
-    server = stubsServer('test');
-    server.create('product');
-    server.create('subscription');
-  });
-
-  afterAll(() => {
-    server.shutdown();
-  });
-
-  it('should show an error message if there is an error', () => {
-    const { getByTestId } = renderWithProviders(<OrderPageContainer />, {
-      cart: {
-        hasError: true,
-      },
-    });
-
-    expect(getByTestId('error-message')).toBeInTheDocument();
-  });
-
-  it('should show an error message if there is an empty cart', () => {
-    const { getByTestId, getByText } = renderWithProviders(<OrderPageContainer />, {
-      cart: {
-        collection: {
-          items: [],
+  describe('states', () => {
+    it('should show an error message if there is an error', () => {
+      const { getByTestId } = renderWithProviders(<OrderPageContainer />, {
+        cart: {
+          hasError: true,
         },
-      },
+      });
+
+      expect(getByTestId('error-message')).toBeInTheDocument();
     });
 
-    expect(getByTestId('error-message')).toBeInTheDocument();
-    expect(getByText(/shopping cart is empty/i)).toBeInTheDocument();
-  });
+    it('should show an error message if there is an empty cart', () => {
+      const { getByTestId, getByText } = renderWithProviders(<OrderPageContainer />, {
+        cart: {
+          collection: {
+            items: [],
+          },
+        },
+      });
 
-  it('should show an error message if there is no cart', () => {
-    const { getByTestId, getByText } = renderWithProviders(<OrderPageContainer />, {
-      cart: {
-        collection: undefined,
-      },
+      expect(getByTestId('error-message')).toBeInTheDocument();
+      expect(getByText(/shopping cart is empty/i)).toBeInTheDocument();
     });
 
-    expect(getByTestId('error-message')).toBeInTheDocument();
-    expect(getByText(/shopping cart is empty/i)).toBeInTheDocument();
+    it('should show an error message if there is no cart', () => {
+      const { getByTestId, getByText } = renderWithProviders(<OrderPageContainer />, {
+        cart: {
+          collection: undefined,
+        },
+      });
+
+      expect(getByTestId('error-message')).toBeInTheDocument();
+      expect(getByText(/shopping cart is empty/i)).toBeInTheDocument();
+    });
   });
 
-  it('should fetch a cart if there is no product selection', () => {
-    (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce('cart-id');
-    (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: null, variantId: null });
-    const fetchCartSpy = jest.spyOn(cartDuck, 'fetchCart');
+  describe('fetch and add to cart', () => {
+    it('should fetch a cart if there is no product selection', () => {
+      (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce('cart-id');
+      (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: null, variantId: null });
+      const fetchCartSpy = jest.spyOn(cartDuck, 'fetchCart');
 
-    const { getByTestId } = renderWithProviders(<OrderPageContainer />);
+      const { getByTestId } = renderWithProviders(<OrderPageContainer />);
 
-    expect(fetchCartSpy).toHaveBeenCalledWith('cart-id');
-    expect(getByTestId('loader')).toBeInTheDocument();
+      expect(fetchCartSpy).toHaveBeenCalledWith('cart-id');
+      expect(getByTestId('loader')).toBeInTheDocument();
+    });
+
+    it('should add an item to a cart if there is a product selection and a cart', () => {
+      (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce('cart-id');
+      (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: 'subscription-id', variantId: 'variant-id' });
+      const addCartItemSpy = jest.spyOn(cartDuck, 'addCartItem');
+
+      const { getByTestId } = renderWithProviders(<OrderPageContainer />);
+
+      expect(addCartItemSpy).toHaveBeenCalledWith({ subscriptionId: 'subscription-id', variantId: 'variant-id' }, 'cart-id');
+      expect(StorageUtil.clearProductSelection).toHaveBeenCalled();
+      expect(getByTestId('loader')).toBeInTheDocument();
+    });
+
+    it('should add an item to a cart if there is a product selection and no cart', () => {
+      (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce(null);
+      (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: 'subscription-id', variantId: 'variant-id' });
+      const addCartItemSpy = jest.spyOn(cartDuck, 'addCartItem');
+
+      const { getByTestId } = renderWithProviders(<OrderPageContainer />);
+
+      expect(addCartItemSpy).toHaveBeenCalledWith({ subscriptionId: 'subscription-id', variantId: 'variant-id' }, undefined);
+      expect(StorageUtil.clearProductSelection).toHaveBeenCalled();
+      expect(getByTestId('loader')).toBeInTheDocument();
+    });
   });
 
-  it('should add an item to a cart if there is a product selection and a cart', () => {
-    (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce('cart-id');
-    (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: 'subscription-id', variantId: 'variant-id' });
-    const addCartItemSpy = jest.spyOn(cartDuck, 'addCartItem');
+  describe('cart', () => {
+    beforeAll(() => {
+      server = stubsServer('test');
+      server.create('product');
+      server.create('subscription');
 
-    const { getByTestId } = renderWithProviders(<OrderPageContainer />);
-
-    expect(addCartItemSpy).toHaveBeenCalledWith({ subscriptionId: 'subscription-id', variantId: 'variant-id' }, 'cart-id');
-    expect(StorageUtil.clearProductSelection).toHaveBeenCalled();
-    expect(getByTestId('loader')).toBeInTheDocument();
-  });
-
-  it('should add an item to a cart if there is a product selection and no cart', () => {
-    (StorageUtil.getCartId as jest.Mock).mockReturnValueOnce(null);
-    (StorageUtil.getProductSelection as jest.Mock).mockReturnValueOnce({ subscriptionId: 'subscription-id', variantId: 'variant-id' });
-    const addCartItemSpy = jest.spyOn(cartDuck, 'addCartItem');
-
-    const { getByTestId } = renderWithProviders(<OrderPageContainer />);
-
-    expect(addCartItemSpy).toHaveBeenCalledWith({ subscriptionId: 'subscription-id', variantId: 'variant-id' }, undefined);
-    expect(StorageUtil.clearProductSelection).toHaveBeenCalled();
-    expect(getByTestId('loader')).toBeInTheDocument();
-  });
-
-  it('should show a cart', () => {
-    const { getByTestId } = renderWithProviders(<OrderPageContainer />, {
-      cart: {
-        collection: {
-          items: [{
-            product: server.db.products[0],
-            subscription: server.db.subscriptions[0],
-            totals: {
-              monthly_price: '',
-              onetime_price: '',
-            },
-          }],
+      cart = {
+        id: 'cart-id',
+        items: [{
+          id: 'item-id',
+          product: server.db.products[0],
+          subscription: server.db.subscriptions[0],
           totals: {
             monthly_price: '',
             onetime_price: '',
           },
+        }],
+        totals: {
+          monthly_price: '',
+          onetime_price: '',
         },
-      },
+      };
     });
 
-    expect(getByTestId('order-page')).toBeInTheDocument();
+    afterAll(() => {
+      server.shutdown();
+    });
+
+    it('should show a cart', () => {
+      const { getByTestId } = renderWithProviders(<OrderPageContainer />, {
+        cart: {
+          collection: cart,
+        },
+      });
+
+      expect(getByTestId('order-page')).toBeInTheDocument();
+    });
+
+    it('should store the cartId', () => {
+      renderWithProviders(<OrderPageContainer />, {
+        cart: {
+          collection: cart,
+        },
+      });
+
+      expect(StorageUtil.setCartId).toHaveBeenCalled();
+    });
+
+    // TODO: remove, return, order
   });
 });
