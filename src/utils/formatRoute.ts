40b84072f2d6f12aab654f7d84731791956932e5
robertschaap <robert.schaap@mac.com>
2020-10-31 11:02:17 +0100

    feat(formatRoute): add comment

--- src/utils/formatRoute.ts
+++ src/utils/formatRoute.ts
@@ -2,6 +2,10 @@ interface RouteParams {
   [key: string]: string | number;
 }
 
+/**
+ * Takes a route string with parameters formatted as `:param` and maps the corresponding
+ * values from the params object (`{ param: 'value' }`) to them
+ */
 const formatRoute = (route: string, params: RouteParams): string => {
   return Object.entries(params).reduce((formattedRoute, [key, value]) => {
     return formattedRoute.replace(`:${key}`, value.toString());
