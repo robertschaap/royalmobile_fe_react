3e2a70349142a954b763efe62fc418ddc92298a5
robertschaap <robert.schaap@mac.com>
2020-10-31 11:02:42 +0100

    feat(StringUtil): add comments

--- src/utils/StringUtil.ts
+++ src/utils/StringUtil.ts
@@ -2,12 +2,18 @@ const capitalise = (string: string): string => {
   return string[0].toUpperCase() + string.slice(1);
 };
 
+/**
+ * Format string as price with two decimals (`0,00`)
+ */
 const formatPrice = (string: string): string => {
   const [number, decimal = ''] = string.split(',');
 
-  return `${number || '0'},${decimal.padEnd(2, '0')}`;
+  return `${number || 0},${decimal.padEnd(2, '0')}`;
 };
 
+/**
+ * Format string as price without decimals (`0,-`)
+ */
 const formatRoundedPrice = (string: string): string => {
   const [number] = string.split(',');
 
