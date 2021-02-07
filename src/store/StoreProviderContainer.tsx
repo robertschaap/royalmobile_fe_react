8c7fb09b31565aac96812851f392cfa6c8a0b621
robertschaap <robert.schaap@mac.com>
2021-02-07 09:57:50 +0100

    feat(StoreProviderContainer): add comment to creatStore function

--- src/store/StoreProviderContainer.tsx
+++ src/store/StoreProviderContainer.tsx
@@ -6,6 +6,13 @@ import { createStore as createReduxStore, applyMiddleware, compose } from 'redux
 import { rootReducer } from './reducers';
 import rootSaga from './sagas';
 
+/**
+ * `createStore` provisions the store, sets up reducers and applies saga
+ * middleware and Redux devtools. This is exported only to enable
+ * store provision for testing purposes It should not be used
+ * anywhere else outside of `src/store` since the
+ * `StoreProviderContainer` implements it.
+ */
 export const createStore = (customInitialState?: Record<string, unknown>) => {
   const sagaMiddleware = createSagaMiddleware();
 
