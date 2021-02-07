0a4b6b1f93f535050e9ecddf39afafbea8000c36
robertschaap <robert.schaap@mac.com>
2021-02-07 10:05:24 +0100

    feat(store): explicit exports for used actions and selectors only

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,33 @@
-export * from './ducks/cart';
-export * from './ducks/product';
-export * from './ducks/products';
-export * from './ducks/subscriptions';
+// Add selectors and actions that need to be export below
+// Do not expose an entire file, the application doesn't need to know.
+
+export {
+  addCartItem,
+  deleteCartItem,
+  fetchCart,
+  placeOrder,
+  selectCart,
+} from './ducks/cart';
+
+export {
+  fetchProduct,
+  invalidateProductCollection,
+  selectProduct,
+  selectProductSelection,
+  setProductSelectedDurationId,
+  setProductSelectedSubscriptionId,
+  setProductSelectedVariantId,
+} from './ducks/product';
+
+export {
+  fetchProducts,
+  selectProducts,
+} from './ducks/products';
+
+export {
+  fetchSubscriptions,
+  selectSubscriptions,
+} from './ducks/subscriptions';
 
 export {
   StoreProviderContainer,
