b150376f9ca7afde84113c514341b5debc5dca6c
robertschaap <robert.schaap@mac.com>
2021-07-03 11:43:09 +0200

    feat(ContactPage): add content

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -33,6 +33,7 @@ const content: Content = Object.freeze({
   'benefits.doubleData': 'Double data in your bundle plus a 5 euro discount when you combine with Royal Internet',
 
   'contact.contactUs': 'Contact us',
+  'contact.info': 'We can’t have you contacting us directly because phone lines cost money to operate, but feel free to drop by in one of our stores or reach out to us on social media.',
 
   'order.emptyCart': 'Your shopping cart is empty',
   'order.finaliseOrder': 'Finalise my order!',
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -17,7 +17,7 @@ const ContactPage: React.FC = () => {
         <SectionHeader>{ContentUtil('contact.contactUs')}</SectionHeader>
         <GridBase>
           <S.ContactInfo md={2} lg={2}>
-            <p>We can’t have you contacting us directly because phone lines cost money to operate, but feel free to drop by in one of our stores or reach out to us on social media.</p>
+            <p>{ContentUtil('contact.info')}</p>
             <S.SocialIcons>
               <li><S.SocialIcon href="https://www.twitter.com/royalmobile"><IconTwitter /></S.SocialIcon></li>
               <li><S.SocialIcon href="https://www.facebook.com/royalmobile"><IconFacebook /></S.SocialIcon></li>
